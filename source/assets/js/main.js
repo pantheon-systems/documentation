22aa2b087c1c2de68e95003ac0e8dd10c8106c8f
Brian MacKinney <brian@pantheon.com>
2014-12-01 13:26:02 -0800

    the rest of adding tocify

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,1 +1,5 @@
-
+  //Executes your code when the DOM is ready.  Acts the same as $(document).ready().
+  $(function() {
+      //Calls the tocify method on your HTML div.
+      $("#toc").tocify();
+  });
