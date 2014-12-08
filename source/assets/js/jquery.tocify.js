4d55cbafa83e97ba3d9fa3a7d4bb45d81adf996e
Brian MacKinney <brian@getpantheon.com>
2014-12-08 13:53:19 -0800

    Convert TOC to behave like getbootrap.com docs

--- source/assets/js/jquery.tocify.js
+++ source/assets/js/jquery.tocify.js
@@ -85,7 +85,7 @@
 
             // **scrollTo**: Accepts Number (pixels)
             // The amount of space between the top of page and the selected table of contents item after the page has been scrolled
-            scrollTo: 75,
+            scrollTo: 50,
 
             // **showAndHideOnScroll**: Accepts a boolean: true or false
             // Determines if table of contents nested items should be shown and hidden while scrolling
@@ -97,7 +97,7 @@
 
             // **highlightOffset**: Accepts a number
             // The offset distance in pixels to trigger the next active table of contents item
-            highlightOffset: 5,
+            highlightOffset: 50,
 
             // **theme**: Accepts a string: "bootstrap", "jqueryui", or "none"
             // Determines if Twitter Bootstrap, jQueryUI, or Tocify classes should be added to the table of contents
