8bb0b589973e2db00d73ff06b408bd4eb5d391a4
Brian MacKinney <brian@getpantheon.com>
2015-04-14 16:18:57 -0700

    Deploy script now local and easy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-SITE_UUID="739913fa-0df2-4766-ac99-4ac57c819f28"
-cd ..
-git clone ssh://codeserver.dev.$SITE_UUID@codeserver.dev.$SITE_UUID.drush.in:2222/~/repository.git get-pantheon-v3
-cd get-pantheon-v3
+cd ../panther
 git checkout docs
-cd ..
-cd documentation
-cd documentation sculpin generate --env=prod 
-mv output_prod/docs ~get-pantheon-v3/
-cd ../get-pantheon-v3
+git pull origin docs
+rm -rf docs/
+cd ../documentation
+rm -rf output_prod/docs
+sculpin generate --env=prod
+mv output_prod/docs ~/sites/panther/
+cd ../panther
 git add --all
-git commit -m “Deploy build x.y.”
-git push origin docs 
+git commit
