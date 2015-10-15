459e6c22c310e44565cac6a1ca113e044ad6eee6
Rachel <rachel@getpantheon.com>
2015-10-15 11:22:40 -0500

    Deploy scripts - rsync backups to filesystem - Deploys to docsfiles Multidev instead of docs (symlink already exists, better testing with existing marketing content) - Write to files/ instead of app server - Correct tmp dir path for rsync uploads - Correct revert script - remote file path for pulling backups down loacally (add /files since backups are now being written to file systems)
    Create env specific revert scripts, create new multidev env for testing up to date content of live

--- scripts/deploy-multidev.sh
+++ scripts/deploy-multidev.sh
@@ -2,7 +2,7 @@
 # Note: Clear cache on $ENV after executing this script
 
 # Define env and site UUID
-export ENV=live
+export ENV=docs-files
 export SITE=72e163bd-0054-4332-8bf8-219c50b78581
 
 
@@ -13,7 +13,7 @@ sculpin generate --env=prod
 # Create local directory and file to log rsync output (local path: "$HOME/sites/docs-backups"), then open for review in real-time
 mkdir ../docs-backups
 mkdir ../docs-backups/`date +%F-%I%p`
-echo "rsync log - deploy to Live environment on `date +%F-%I%p`" > ../docs-backups/`date +%F-%I%p`/rsync-`date +%F-%I%p`.log
+echo "rsync log - deploy to docsfiles Multidev environment on `date +%F-%I%p`" > ../docs-backups/`date +%F-%I%p`/rsync-`date +%F-%I%p`.log
 open ../docs-backups/`date +%F-%I%p`/rsync-`date +%F-%I%p`.log
 
 
@@ -23,4 +23,4 @@ rsync -bv --backup-dir=docs-backups/`date +%F-%I%p` --log-file=../docs-backups/`
 
 # Send the rysnc log file to remote directory "/docs-backups/`date +%F-%I%p`/"
 rsync -rlvz --temp-dir=../../../tmp/ --size-only --progress -e 'ssh -p 2222' ../docs-backups/`date +%F-%I%p`/rsync-`date +%F-%I%p`.log $ENV.$SITE@appserver.$ENV.$SITE.drush.in:files/docs-backups/`date +%F-%I%p`
-open https://pantheon.io/docs/
+open http://docs-files-panther.pantheon.io/docs
--- scripts/revert-multidev.sh
+++ scripts/revert-multidev.sh
@@ -8,12 +8,13 @@
 #       Note: Clear cache on $ENV after executing this script           #
 
 # Define env and site UUID
-export ENV=live
+export ENV=docs-files
 export SITE=72e163bd-0054-4332-8bf8-219c50b78581
 
 # Pull backup down from app server
-rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222' $ENV.$SITE@appserver.$ENV.$SITE.drush.in:docs-backups/`date +%F-%I%p`/ ../docs-backups/`date +%F-%I%p`/
+rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222' $ENV.$SITE@appserver.$ENV.$SITE.drush.in:files/docs-backups/`date +%F-%I%p`/ ../docs-backups/`date +%F-%I%p`/
 
 
 # Restore backup to to $ENV.$SITE@appserver.$ENV.$SITE.drush.in:files/docs
 rsync -rlvza --temp-dir=../../tmp/ --delete-after --ipv4 --progress -e 'ssh -p 2222' ../docs-backups/`date +%F-%I%p`/docs/* $ENV.$SITE@appserver.$ENV.$SITE.drush.in:files/docs
+open http://docs-files-panther.pantheon.io/docs
