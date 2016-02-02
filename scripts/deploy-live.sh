81262b32dd37510a988393532dc9172571136093
Rachel <rachel@getpantheon.com>
2016-02-02 13:50:57 -0600

    Update vm with current remote branches
    This will avoid failures on master where branches do not exist and deletion is attempted

--- scripts/deploy-live.sh
+++ scripts/deploy-live.sh
@@ -56,7 +56,8 @@ getExistingTerminusEnvs() {
 }
 getExistingTerminusEnvs "filtered_env_list.txt"
 
-
+# Update vm with current remote branches
+git remote update origin --prune
 # Identify merged remote branches, ignoring Pantheon defaults and master
 git branch -r --merged master | awk -F'/' '/^ *origin/{if(!match($0, /(>|master)/) && (!match($0, /(>|dev)/)) && (!match($0, /(>|test)/)) && (!match($0, /(>|live)/))){print $2}}' | xargs -0 > merged-branches.txt
 # Delete empty line at the end of txt file produced by awk
