b94202f42ff618aa1e63c21da0f4a939fca13e44
Steve Persch <stevector@users.noreply.github.com>
2016-02-18 11:19:10 -0600

    adding sitespeed test
    whitespace
    
    trying to resolve circle error
    
    trying to resolve circle error
    
    trying to resolve circle error with phantom js
    
    whitespace
    
    fixing circle error
    
    trying to add firefox as the browser for sitespeed.io
    
    trying sitespeed against the live site
    
    Adding budget file
    
    Testing against performance budget
    
    Switching back to testing the local build
    
    Adding skipping of some tests just to show that it can be done. Unsure which tests should actually be skipped
    
    Lowering performance budget testing threshold to 60 just to see the change
    
    whitespace
    
    Removing and rearranging some JS to see what happens to the scores
    
    Revert "Removing and rearranging some JS to see what happens to the scores"
    
    This reverts commit 57fda88c591de24ca88b517f282eeaebde5c3f97.
    
    Adding comments
    
    Adding comments
    
    restoring deploy to multidev
    
    whitespace change to trigger another circle build
    
    trying to install phantomjs with npm
    
    commenting out deploy to multidev
    
    switching install of sitespeed (and thus phantom) to package.json
    
    Moving sitespeed to script
    
    Uncommenting deploy script
    
    hard-coding multidev URL to confirm sitespeed results
    
    Constructing multidev url
    
    Adding a compiled CSS file
    
    Updating package.json will new dependencies
    
    Setting up concat in grunt file
    
    Using new compiled.css
    
    Adding component to package.json
    
    Updating gitignore file
    
    Compiled CSS
    
    Adding a todo
    
    adding grunt-copy
    
    Grunt file in progress
    
    Grunt file cleanup
    
    Adding grunt-css-url-replace
    
    Grunt file updated
    
    switching highlight css to a node module
    
    switching highlight css to a node module
    
    Switching font awesome to compiled css
    
    Committing compiled CSS. Might revert this
    
    CSS concatenation on taxonomy page
    
    Small change to trigger a rerun of sitespeed
    
    Adding list of urls to test
    
    Updating sitespeed script to use list of urls
    
    Adding more urls to be tested
    
    Gruntfile cleanup
    
    Removing and rearranging some JS to see what happens to the scores
    
    Using new compiled.css
    
    CSS concatenation on taxonomy page
    
    Updating circle.yml for grunt changes
    
    Update circle.yml
    
    Fixing typo in Gruntfile
    
    Update run-sitespeed-test.sh
    
    Changing top-level domain to be checked
    
    whitespace changes
    
    Removing unused package
    
    Undoing change in footer not wanted for this branch
    
    Adding a profile live as a point of comparison
    
    restoring JS to header that should not have been removed
    
    Update to sitespeed test
    
    Whitespace
    
    whitepace
    
    Deleting compiled css
    
    Adding compiled css to gitignore
    
    Trying to speed up the running of sitespeed
    
    whitespace
    
    whitespace
    
    Making the budget easy to pass
    
    switching back to 3 runs for sitespeed
    
    Removing hard-coded vars
    
    Turning up budget numbers
    
    Update to logic to derive multidev url

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,33 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    copy: {
+      main: {
+        files: [
+          {expand: true, src: ['node_modules/bootstrap/dist/**'], dest: 'source/docs/assets/'},
+          {expand: true, src: ['node_modules/highlight.js/styles/tomorrow-night-eighties.css'], dest: 'source/docs/assets/'},
+          {expand: true, src: ['node_modules/node-font-awesome/node_modules/font-awesome/css/font-awesome.min.css'], dest: 'source/docs/assets/'},
+          {expand: true, src: ['node_modules/node-font-awesome/node_modules/font-awesome/fonts/**'], dest: 'source/docs/assets/'}
+        ],
+      },
+    },
+    css_url_replace: {
+      options: {
+        staticRoot: 'source'
+      },
+      replace: {
+        files: {
+          'source/docs/assets/compiled/compiled.css': [
+            'source/docs/assets/node_modules/highlight.js/styles/tomorrow-night-eighties.css',
+            'source/docs/assets/node_modules/node-font-awesome/node_modules/font-awesome/css/font-awesome.min.css',
+            'source/docs/assets/node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'source/docs/assets/node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+            'source/docs/assets/css/main.css',
+            'source/docs/assets/fonts/pan-docs-icons/css/pan-docs.css',
+            'source/docs/assets/css/jquery.tocify.css'
+          ],
+        }
+      }
+    },
     a11y: {
       dev: {
         options: {
@@ -21,6 +49,11 @@ module.exports = function(grunt) {
       }
     }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-css-url-replace');
   grunt.loadNpmTasks('grunt-a11y');
-  grunt.registerTask('default', ['a11y']);
+
+  grunt.registerTask('default', ['copy', 'css_url_replace']);
+  grunt.registerTask('test', ['a11y']);
 }
