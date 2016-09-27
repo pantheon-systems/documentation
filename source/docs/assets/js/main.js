b63a50fecd25d2f2c8c7d944ee7ccd84fee96d7c
Rachel <rachel@getpantheon.com>
2016-09-27 12:07:45 -0500

    Revert "First pass form selector for OS terminus/install.md"
    This reverts commit 46857f8e68f6ee35cb9a79c46cfc18115800d0e0.

--- source/docs/assets/js/main.js
+++ source/docs/assets/js/main.js
@@ -94,9 +94,3 @@
     return filtered;
   }
 });
-
-
-  //Terminus Install Form Selector & Toggle
-  $('#osSelect').on('change', function (e) {
-    $('#myOS li a').eq($(this).val()).tab('show');
-});
