7d6455ab27b84c7f5d309dc8f95b179c4a01a2ae
alexfornuto <alex.fornuto@pantheon.io>
2018-05-08 22:20:15 -0700

    Squashed Commit, details below.
    EUREKA!
    
    add grunt watch to app.sh
    
    comment composer self-update
    
    The idea being that the parent docker image will maintain the Composer version.

--- app.sh
+++ app.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Start the Sculpin server
-bin/sculpin server
+#bin/sculpin server
+
+# Instead, use the watch.php script and a basic server
+grunt watch && php watch.php & php -S0.0.0.0:8000 -t output_dev/
