1e0a849ed13e3c26bf393a1dd51519187de936bd
alexfornuto <alex.fornuto@pantheon.io>
2018-05-14 12:40:40 -0400

    Squashed work on Docker container and scripts
    reorder and separate Dockerfile commands
    
    rebuild runthedocs script to be more docker-aware
    
    moved scripts into proper dir and updated for docker
    
    consolidate runthedocs script
    
    add script files moved without git mv
    
    update README and CONTRIBUTING
    
    fix changelog fix script
    
    script fixes

--- runthedocs.sh
+++ runthedocs.sh
@@ -1,6 +1,13 @@
 #/bin/bash
 
-# Invoke the docker image with a shared volume for source and ability to quit with CTRL+C
 
-docker run -p 8000:8000 -v /home/alex/repos/documentati/source:/documentation/source -it documentation
+# Invoke the docker image with a shared volume for source.
+if docker run -p 8000:8000 --name=pantheon-docs -v $(pwd)/source:/documentation/source -dit pantheonsystems/documentation; then
+    printf "Container built and running \n"
+else
+    printf "Container exists... \n"
+    docker container start pantheon-docs
+fi
 
+# Start app script, which invokes grunt, the watch script, and a simple PHP web server
+docker exec pantheon-docs /documentation/scripts/app.sh
