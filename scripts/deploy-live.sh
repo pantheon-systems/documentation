edce11a11d6cf1cb03dac056a83e48c87f345cbc
alexfornuto <alex.fornuto@pantheon.io>
2019-03-15 11:58:50 -0500

    edits to comments

--- scripts/deploy-live.sh
+++ scripts/deploy-live.sh
@@ -14,7 +14,7 @@ do
   mkdir -p output_prod/docs/changelog/page/"$name"
   mv "$file" "output_prod/docs/changelog/page/"$name"/index.html"
 done
-# Lower search ranking for individual changelog files
+# Lower search ranking for changelog backscroll
 for file in output_prod/docs/changelog/page/*/index.html
 do
   sed -i '61i\'"        <meta name=\"addsearch-boost\" content=\"30\" />"'\' $file
