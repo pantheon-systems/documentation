5e7c2ddc565136a4f35647c509bff873a59c2577
Luis Dueñas <luis.alonso.duenas.m@gmail.com>
2019-05-17 09:45:31 -0700

    fix: add missing props (#29)

--- gatsby/src/components/twitter.js
+++ gatsby/src/components/twitter.js
@@ -1,7 +1,6 @@
 import React from "react"
 
-const Twitter = () => {
-    
+const Twitter = ({ pageTitle, path }) => {
   return (
     <iframe
       id="twitter-widget-0"
@@ -10,15 +9,15 @@ const Twitter = () => {
       allowtransparency="true"
       className="twitter-share-button twitter-share-button-rendered twitter-tweet-button"
       title="Twitter Tweet Button"
-      src="https://platform.twitter.com/widgets/tweet_button.fb066ff7f5f4afee7716887031da2ea8.en.html#dnt=false&amp;
-        id=twitter-widget-0&amp;
-        lang=en&amp;
-        original_referer=https%3A%2F%2Fpantheon.io%2Fdocs%2Fredis%2F&amp;
-        size=m&amp;
-        text=%40getpantheon%20doc%3A%20Installing%20Redis%20on%20Drupal%20or%20WordPress&amp;
-        time=1558046480896&amp;
-        type=share&amp;
-        url=https%3A%2F%2Fpantheon.io%2Fdocs%2Fredis%2F"
+      src={`https://platform.twitter.com/widgets/tweet_button.fb066ff7f5f4afee7716887031da2ea8.en.html#dnt=false;
+        id=twitter-widget-0;
+        lang=en;
+        original_referer=https%3A%2F%2Fpantheon.io${path};
+        size=m;
+        text=%40getpantheon%20doc%3A%20${pageTitle};
+        time=${Date.now()};
+        type=share;
+        url=https%3A%2F%2Fpantheon.io${path}`}
       style={{ width: 60, height: 25 }}
     />
   )
