431e76e15e251a51ed56a8340a16e10fa8bcf21e
Luis Dueñas <luis.alonso.duenas.m@gmail.com>
2019-05-17 17:11:10 -0700

    refactor: upgrade Accordion class component to hook (#37)

--- gatsby/src/components/accordion.js
+++ gatsby/src/components/accordion.js
@@ -1,53 +1,36 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 
-class Accordion extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { isCollapsed: false }
-  }
+const Accordion = ({ id, icon, title, children }) => {
+  const [isCollapsed] = useState(false)
 
-  collapseAccordion = () => {
-    var oldValue = this.state.isCollapsed
-    this.setState({
-      ...this.state,
-      isCollapsed: !oldValue,
-    })
-  }
-  render() {
-    let classCollapsed = this.state.isCollapsed ? "collapse in" : "collapse"
-    return (
-      <>
-        <div className="panel panel-drop panel-guide" id="accordion">
-          <div className="panel-heading panel-drop-heading">
-            <a
-              href={`#${this.props.id}`}
-              className={`accordion-toggle panel-drop-title ${
-                this.state.isCollapsed ? "" : "collapsed"
-              }`}
-              data-toggle={classCollapsed}
-              data-parent="#accordion"
-              data-proofer-ignore=""
-              data-target={`#${this.props.id}`}
-              aria-expanded="false"
-              onClick={this.collapseAccordion}
-            >
-              <h3 className="panel-title panel-drop-title info">
-                <span className={`glyphicons glyphicons-${this.props.icon}}`} />
-                {this.props.title}
-              </h3>
-            </a>
-          </div>
-          <div
-            id={`${this.props.id}`}
-            className={classCollapsed}
+  const classCollapsed = isCollapsed ? "collapse in" : "collapse"
+  return (
+    <>
+      <div className="panel panel-drop panel-guide" id="accordion">
+        <div className="panel-heading panel-drop-heading">
+          <a
+            href={`#${id}`}
+            className={`accordion-toggle panel-drop-title ${
+              isCollapsed ? "" : "collapsed"
+            }`}
+            data-toggle={classCollapsed}
+            data-parent="#accordion"
+            data-proofer-ignore=""
+            data-target={`#${id}`}
             aria-expanded="false"
           >
-            <div className="panel-inner">{this.props.children}</div>
-          </div>
+            <h3 className="panel-title panel-drop-title info">
+              <span className={`glyphicons glyphicons-${icon}`} />
+              {title}
+            </h3>
+          </a>
         </div>
-      </>
-    )
-  }
+        <div id={`${id}`} className={classCollapsed} aria-expanded="false">
+          <div className="panel-inner">{children}</div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Accordion
