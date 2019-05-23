a26cb740b269773775cd89e6213a62b437e462d7
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2019-05-23 06:16:30 -0700

    feat: add content to index page (#67)

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -7,18 +7,59 @@ class Index extends React.Component {
   render() {
     return (
       <Layout>
-        <div className="container">
+        <div style={{ marginTop: "-20px" }} className="container">
           <div className="row doc-content-well">
-            <h1>No landing page yet</h1>
-            <h4>Try accesing one of the available pages:</h4>
-            <ul>
-              <li>
-                <Link to="/docs/redis">Redis</Link>
-              </li>
-              <li>
-                <Link to="/docs/terminus">Terminus</Link>
-              </li>
-            </ul>
+            <div className="row">
+              <h1 className="title">Documentation Topics</h1>
+            </div>
+            <div className="row" style={{ marginBottom: "15px" }}>
+              <div className="col-md-12">
+                <a
+                  className="topic-info-link"
+                  style={{ display: "block! important" }}
+                  href="#"
+                >
+                  <div
+                    className="guide-cta-landing"
+                    style={{ padding: "25px !important", display: "block" }}
+                  >
+                    <h1 className="hero-video__guide-cta-title">
+                      Quick Start Guide{" "}
+                      <i
+                        className="fa fa-angle-right"
+                        style={{ color: "#EFD01B" }}
+                      />
+                    </h1>
+                    <p className="hero-video__guide-cta-subtitle">
+                      New to our platform? Check out our step-by-step guide to
+                      learn all the basics.
+                    </p>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="row mb-70">
+              <div class="col-md-12" />
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                <h2 class="subtitle">Popular Documentation</h2>
+              </div>
+            </div>
+            <div class="row mb-70">
+              <div class="col-md-12">
+                <ul class="top-docs top-docs-3col">
+                  <li>
+                    <a href="/docs/terminus/">Terminus Manual</a>
+                  </li>
+                  <li>
+                    <a href="/docs/redis/">
+                      Installing Redis on Drupal or WordPress
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </Layout>
