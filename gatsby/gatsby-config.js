976803af19e1eaf973c87ea118740a722e5f735b
alexfornuto <alex.fornuto@pantheon.io>
2019-05-31 14:17:55 -0500

    add Google Tracking ID

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -91,7 +91,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-20272439-10`,
       },
     },
     `gatsby-plugin-react-helmet`,
