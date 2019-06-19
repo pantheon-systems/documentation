a4a120c83eb4e505323d4e2ff54f795da5816d0d
Jairo Rondon <jimnoname@gmail.com>
2019-06-19 17:07:57 -0500

    feat: create home page components and data for home and landing pages

---
+++ gatsby/src/layout/three-column-list/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'gatsby';
+
+const propTypes = {
+  title: PropTypes.string,
+  links: PropTypes.array,
+}
+
+function ThreeColumnList(props) {
+  const {title, links} = props;
+  console.log(links);
+  return (
+    <React.Fragment>  
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="subtitle">{title}</h2>
+        </div>
+      </div>
+      <div class="row mb-70">
+        <div class="col-md-12">
+          <ul class="top-docs top-docs-3col">
+            {links.map((link)=>(
+              <li key={link.url}>
+                <Link to={link.url}>{link.text}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+ThreeColumnList.propTypes = propTypes
+
+export default ThreeColumnList
