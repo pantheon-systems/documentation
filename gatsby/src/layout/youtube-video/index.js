1da4f9128904e9e3a67226e8ea6b401607570c2b
alexfornuto <alex.fornuto@pantheon.io>
2019-06-21 17:41:24 -0500

    additional video embeds

--- gatsby/src/layout/youtube-video/index.js
+++ gatsby/src/layout/youtube-video/index.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 class YoutubeVideo extends Component {
 	render() {
 		const { videoURL } = this.props;
-    const source = 'https://www.youtube.com/embed/${videoURL}';
 		return (
 			<div>
         <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + this.props.videoURL} frameborder="0" allow="accelerometer; encrypted-media; gyroscope;" allowfullscreen></iframe>
