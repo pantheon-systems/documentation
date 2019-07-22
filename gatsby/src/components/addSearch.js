0cbfa8b8ba1eec61eb84e8b268fa72f1689f7713
Dustin Schau <dustinschau@gmail.com>
2019-07-22 13:51:20 -0700

    chore: only append script tag on launch

--- gatsby/src/components/addSearch.js
+++ gatsby/src/components/addSearch.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import {Helmet} from "react-helmet";
 
 class AddSearch extends React.Component {
+    componentDidMount() {
+        const script = document.createElement('script')
+        script.setAttribute('src', 'https://addsearch.com/js/?key=a7b957b7a8f57f4cc544c54f289611c6')
+        script.setAttribute('defer', true)
 
+        document.body.appendChild(script)
+    }
     render () {
         return (
-            <div className="addsearch-container">
-                <Helmet>
-                    <script defer src="https://addsearch.com/js/?key=a7b957b7a8f57f4cc544c54f289611c6"></script>
-                </Helmet>
-            </div>
+            <div className="addsearch-container" />
         );
       }
 
 }
 
 export default AddSearch;
-
-
-                
