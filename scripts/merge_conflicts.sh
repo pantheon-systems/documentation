9339af82617915bb365ccc71dc29e9014825249a
Alex Fornuto <alex.fornuto@pantheon.io>
2019-09-17 10:30:47 -0500

    add merge conflict test into circle config

--- scripts/merge_conflicts.sh
+++ scripts/merge_conflicts.sh
@@ -3,13 +3,13 @@ set -e
 path=$(dirname "$0")
 base=$(cd $path/.. && pwd)
 
-cd $base/output_prod/docs
-if grep -q --exclude="git-faq/index.html" --exclude="core-updates/index.html" --exclude="maintain-custom-upstream/index.html" "&lt;&lt;&lt;&lt;&lt;&lt;&lt; HEAD" $(find . -type f); then
-  echo 'Merge HEAD hash: fail' && exit 1
-elif grep -q --exclude="git-faq/index.html" --exclude="core-updates/index.html" --exclude="maintain-custom-upstream/index.html" "<<<<<<< HEAD" $(find . -type f); then
-  echo 'Merge HEAD hash: fail' && exit 1
-elif grep -q --exclude="core-updates/index.html" --exclude="maintain-custom-upstream/index.html" --exclude="mysql-slow-log/index.html" "=======" $(find . -not -path "./assets/*" -type f ); then
-  echo 'Merge equal signs: fail' && exit 1
+cd $base/source/
+if grep -q --exclude="git-faq.md" --exclude="core-updates.md" --exclude="maintain-custom-upstream.md" ">>>>>>> HEAD" $(find . -type f); then
+  echo 'Merge HEAD hash: fail with >>>>' && exit 1
+elif grep -q --exclude="git-faq.md" --exclude="core-updates.md" --exclude="maintain-custom-upstream.md" "<<<<<<<" $(find . -type f); then
+  echo 'Merge HEAD hash: fail with <<<<' && exit 1
+elif grep -q --exclude="docs/*" --exclude="git-faq.md" --exclude="core-updates.md" --exclude="maintain-custom-upstream.md" --exclude="mysql-slow-log.md" "=======" $(find . -type f ); then
+  echo 'Merge equal signs: fail with ====' && exit 1
 else
   echo 'Merge conflict: pass' && exit 0
 fi
