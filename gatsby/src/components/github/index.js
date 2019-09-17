d9ef8d265bac2eea4de7341999b29b892ae5621e
Alex Fornuto <alex.fornuto@pantheon.io>
2019-09-17 11:12:15 -0500

    fix new issue link

--- gatsby/src/components/github/index.js
+++ gatsby/src/components/github/index.js
@@ -26,7 +26,7 @@ const Github = ({ pageTitle, path, editPath }) => {
         </li>
         <li>
           <a
-            href={`https://github.com/pantheon-systems/documentation/issues/new?title=${pageTitle}%20Doc%20Update%20&body=Re%3A%20%5B${pageTitle}%5D(https%3A%2F%2Fpantheon.io/docs${path})%0A%0APriority%3A%20Low%2FMedium%2FHigh%20(choose%20one%2C%20remove%20the%20other%20options)%0A%0A%23%23%20Issue%20Description%3A%0A%0A%23%23%20Suggested%20Resolution`}
+            href={`https://github.com/pantheon-systems/documentation/issues/new?title=${pageTitle}%20Doc%20Update%20&body=Re%3A%20%5B${pageTitle}%5D(https%3A%2F%2Fpantheon.io/docs/${path})%0A%0APriority%3A%20Low%2FMedium%2FHigh%20(choose%20one%2C%20remove%20the%20other%20options)%0A%0A%23%23%20Issue%20Description%3A%0A%0A%23%23%20Suggested%20Resolution`}
             target="blank"
           >
             Report Doc Issue
