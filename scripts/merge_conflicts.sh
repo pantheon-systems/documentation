49b816159a6f9aebdd3c8fa7d0f04e9a6f03d592
Alex Fornuto <alex.fornuto@pantheon.io>
2019-09-17 11:45:03 -0500

    flip head and tail chars

--- scripts/merge_conflicts.sh
+++ scripts/merge_conflicts.sh
@@ -4,9 +4,9 @@ path=$(dirname "$0")
 base=$(cd $path/.. && pwd)
 
 cd $base/source/
-if grep -q --exclude="git-faq.md" --exclude="core-updates.md" --exclude="maintain-custom-upstream.md" ">>>>>>> HEAD" $(find . -type f); then
+if grep -q --exclude="git-faq.md" --exclude="core-updates.md" --exclude="maintain-custom-upstream.md" "<<<<<<< HEAD" $(find . -type f); then
   echo 'Merge HEAD hash: fail with >>>>' && exit 1
-elif grep -q --exclude="git-faq.md" --exclude="core-updates.md" --exclude="maintain-custom-upstream.md" "<<<<<<<" $(find . -type f); then
+elif grep -q --exclude="git-faq.md" --exclude="core-updates.md" --exclude="maintain-custom-upstream.md" ">>>>>>>" $(find . -type f); then
   echo 'Merge HEAD hash: fail with <<<<' && exit 1
 elif grep -q --exclude="docs/*" --exclude="git-faq.md" --exclude="core-updates.md" --exclude="maintain-custom-upstream.md" --exclude="mysql-slow-log.md" "=======" $(find . -type f ); then
   echo 'Merge equal signs: fail with ====' && exit 1
