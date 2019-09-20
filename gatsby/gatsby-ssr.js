3a28005961ff0de3c88548602566b10dfecde0bd
jmolivas <jesus.olivas@gmail.com>
2019-09-20 13:39:31 -0700

    fix: twitter button renders inconsistently

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -6,6 +6,7 @@ import React from 'react'
 export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
     <script key="jquery" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.1/jquery.min.js" />,
-    <script key="bootstrap" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" />
+    <script key="bootstrap" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" />,
+    <script key="twitter" src="https://platform.twitter.com/widgets.js" />
   ])
 }
