33f10fe2f8bf9b6b373ce10543a92f87978cf9b9
Alex Fornuto <alex.fornuto@pantheon.io>
2019-09-25 17:44:58 -0500

    script improvement

--- scripts/link-checker.sh
+++ scripts/link-checker.sh
@@ -2,9 +2,17 @@
 set -e
 
 # This script assumes you're already serving the site using --prefix-paths
-echo "Reading excluded links..."
-exclusions=$(sed -r 's/(.*)/--exclude \1/' scripts/exclude.txt | tr '\n' ' ')
 
-echo "Checking for broken links:"
-./gatsby/node_modules/broken-link-checker/bin/blc -q -r -e -o ${exclusions} --user-agent='broken-link-checker/0.8.0 Node.js/8.9.4 (OS X; x64)' http://localhost:9000/docs
+# This sections excludes domains, which will come in handy when it's expanded for all links
+#echo "Reading excluded links..."
+#exclusions=$(sed -r 's/(.*)/--exclude \1/' scripts/exclude.txt | tr '\n' ' ')
+
+echo "Checking for broken links.."
+
+if ./gatsby/node_modules/broken-link-checker/bin/blc -q -r -e -o http://localhost:9000/docs
+then
+  echo "No broken internal links!"
+else
+  exit 1
+fi
 
