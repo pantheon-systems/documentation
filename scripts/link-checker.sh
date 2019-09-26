8170da1edfa6892fe9b288e90f290aaffda4bfb4
Alex Fornuto <alex.fornuto@pantheon.io>
2019-09-26 13:19:32 -0500

    separate local and external link scripts

--- scripts/link-checker.sh
+++ scripts/link-checker.sh
@@ -3,10 +3,6 @@ set -e
 
 # This script assumes you're already serving the site using --prefix-paths
 
-# This sections excludes domains, which will come in handy when it's expanded for all links
-#echo "Reading excluded links..."
-#exclusions=$(sed -r 's/(.*)/--exclude \1/' scripts/exclude.txt | tr '\n' ' ')
-
 echo "Checking for broken links.."
 
 if ./gatsby/node_modules/broken-link-checker/bin/blc -q -r -e -o http://localhost:9000/docs
