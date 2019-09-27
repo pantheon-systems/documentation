be7fcbf80dee5b887486864001bc9fa8fbd24a90
Andrew Taylor <andrew@ataylor.me>
2019-09-27 15:37:46 -0700

    Add Switch component

---
+++ gatsby/src/components/Switch/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './index.css';
+
+const Switch = ({
+    isOn,
+    handleToggle,
+    onColor = '#06D6A0',
+    slug = 'react-switch',
+    text = '',
+    required = false
+}) => {
+    return (
+        <div className='react-switch-wrap row form-group' id={`${slug}-wrap`}>
+            <input
+                required={required}
+                checked={isOn}
+                onChange={handleToggle}
+                className="react-switch-checkbox"
+                id={slug}
+                type="checkbox"
+            />
+            <label
+                style={{ background: isOn && onColor }}
+                className="react-switch-label form-label"
+                htmlFor={slug}
+            >
+                <span className={`react-switch-button`} />
+            </label>
+            <span className='switch-text'>{text}</span>
+        </div>
+    );
+};
+
+export default Switch;
