5f35156846740b41ee4167c5553ec5d29fbc5815
Andrew Taylor <andrew@ataylor.me>
2019-09-27 15:37:23 -0700

    Add radio input group component

---
+++ gatsby/src/components/RadioInputGroup.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const RadioInputGroup = ({
+    name = 'radioGroup',
+    label = 'Select a value',
+    value,
+    slug = 'react-radio-group',
+    handleChange,
+    options = [],
+    colSize = 3
+}) => {
+    return (options.length) ? (
+        <fieldset className='react-radio-group' id={`${slug}-wrap`}>
+            <Form.Group as={Row}>
+                <Col sm={12}>
+                    <Form.Label as="legend">
+                        {label}
+                    </Form.Label>
+                </Col>
+                {
+                    options.map(obj =>{ 
+                        return (
+                            <Col sm={colSize}>
+                                <Form.Check
+                                    type="radio"
+                                    label={obj.label}
+                                    value={obj.value}
+                                    name={name}
+                                    id={obj.id}
+                                    checked={value === obj.value}
+                                    onChange={handleChange}
+                                />
+                            </Col>
+                        )
+                     })
+                }
+            </Form.Group>
+        </fieldset>
+    ) : null;
+};
+
+export default RadioInputGroup;
