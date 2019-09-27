271d29c0b8ceb44483cdf0ecdf61f3e2e50bf21a
Alex Fornuto <alex.fornuto@pantheon.io>
2019-09-27 11:43:30 -0500

    add terminus update script to circle config

---
+++ scripts/update-terminus-commands.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# This script is designed to run as part of the CircleCI build process. To run it locally, you must first have $GITHUB_TOKEN defined in your session, and run it from the project root.
+
+echo "Generate a new Terminus commands.json file"
+terminus list --format=json > source/data/commands.json
+
+echo "Update releases"
+curl -v -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/pantheon-systems/terminus/releases > source/data/releases.json
