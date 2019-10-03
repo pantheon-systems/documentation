a1b4d4440bce22573fdce35b00d13864eff2bc45
Alex Fornuto <alex.fornuto@pantheon.io>
2019-10-03 12:52:35 -0500

    no gatsby cache, increment circle cache

--- scripts/deploy-multidev.sh
+++ scripts/deploy-multidev.sh
@@ -89,17 +89,6 @@ if [ "$CIRCLE_BRANCH_SLUG" != "master" ] && [ "$CIRCLE_BRANCH_SLUG" != "dev" ] &
     fi
   done
 
-  printf "Cache: \n"
-  while true
-  do
-    if ! rsync --delete-delay -chrlz --ipv4 --log-file=logs/multidev-log-a-m.txt -e 'ssh -p 2222 -oStrictHostKeyChecking=no' gatsby/public/.cache/ --temp-dir=../../tmp/ $normalize_branch.$STATIC_DOCS_UUID@appserver.$normalize_branch.$STATIC_DOCS_UUID.drush.in:files/docs/; then
-    echo "Failed, retrying..."
-    sleep 5
-  else
-    break
-  fi
-  done
-
 
   printf "\n Commenting on GitHub... \n"
 
