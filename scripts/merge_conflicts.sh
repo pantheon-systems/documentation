0160c7d6467af27aa29541e69e948659dd33b139
Alex Fornuto <alex.fornuto@pantheon.io>
2019-10-21 11:30:40 -0500

    fix conflict check output

--- scripts/merge_conflicts.sh
+++ scripts/merge_conflicts.sh
@@ -5,9 +5,9 @@ base=$(cd $path/.. && pwd)
 
 cd $base/source/
 if grep -q --exclude="git-faq.md" --exclude="core-updates.md" --exclude="maintain-custom-upstream.md" "<<<<<<< HEAD" $(find . -type f); then
-  echo 'Merge HEAD hash: fail with >>>>' && exit 1
+  echo 'Merge HEAD hash: fail with <<<< HEAD' && exit 1
 elif grep -q --exclude="git-faq.md" --exclude="core-updates.md" --exclude="maintain-custom-upstream.md" ">>>>>>>" $(find . -type f); then
-  echo 'Merge HEAD hash: fail with <<<<' && exit 1
+  echo 'Merge HEAD hash: fail with >>>>' && exit 1
 elif grep -q --exclude="docs/*" --exclude="git-faq.md" --exclude="core-updates.md" --exclude="maintain-custom-upstream.md" --exclude="mysql-slow-log.md" "=======" $(find . -type f ); then
   echo 'Merge equal signs: fail with ====' && exit 1
 else
