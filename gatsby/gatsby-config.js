46a93079a50f46cf19fe5063dcd2a6bda129caa9
Alex Fornuto <alex.fornuto@pantheon.io>
2019-10-25 17:33:28 -0500

    Add Segment plugin to Gatsby

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -109,6 +109,14 @@ module.exports = {
             }
           },
           {
+            resolve: "gatsby-plugin-segment-js",
+            options: {
+              prodKey: process.env.SEGMENT_KEY,
+              devKey: process.env.SEGMENT_KEY,
+              trackPage: true,
+            },
+          },
+          {
             resolve: "gatsby-remark-embed-video",
             options: {
               width: 800,
