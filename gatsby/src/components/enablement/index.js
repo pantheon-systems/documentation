5e595c722b9f6377a6c5b5be4d4d57d8a4863f31
Alex Fornuto <alex.fornuto@pantheon.io>
2019-11-14 16:37:18 -0600

    function fix

--- gatsby/src/components/enablement/index.js
+++ gatsby/src/components/enablement/index.js
@@ -2,15 +2,18 @@ import React from "react"
 import './style.css';
 
 const Enablement = ({ title, link, campaign, children }) => {
-  _handleClick() {
-    window.analytics.track("Docs Enablement Clicked", {
-        campaign: {campaign},
-    });
-}
+
+  function _handleClick() {
+    if (window.analytics){
+      window.analytics.track("Docs Enablement Clicked", {
+          campaign: {campaign},
+      });
+    }
+  }
   return (
     <div className="enablement">
       <h4 className="info">
-        <a href={link} class="external" onClick={this._handleClick}>{title}</a>
+        <a href={link} className="external" onClick={_handleClick}>{title}</a>
       </h4>
       {children}
     </div>
