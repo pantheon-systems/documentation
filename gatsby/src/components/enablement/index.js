03ec19a8431ccd2e49c9dc49960b7f9834a250d4
Carolyn Shannon <55147679+carolyn-shannon@users.noreply.github.com>
2019-11-14 12:45:27 -0600

    add segment click tracking to enablement component

--- gatsby/src/components/enablement/index.js
+++ gatsby/src/components/enablement/index.js
@@ -1,11 +1,16 @@
 import React from "react"
 import './style.css';
 
-const Enablement = ({ title, link, children }) => {
+const Enablement = ({ title, link, campaign, children }) => {
+  _handleClick() {
+    window.analytics.track("Docs Enablement Clicked", {
+        campaign: {campaign},
+    });
+}
   return (
     <div className="enablement">
       <h4 className="info">
-        <a href={link} class="external">{title}</a>
+        <a href={link} class="external" onClick={this._handleClick}>{title}</a>
       </h4>
       {children}
     </div>
