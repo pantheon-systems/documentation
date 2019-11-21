0a3ec7f19d6f5b567f67d12fb870aa8448f2ffdd
Alex Fornuto <alex.fornuto@pantheon.io>
2019-11-21 13:36:07 -0600

    cut throughput to 1 Mbps SKIP_PREVIEW

--- gatsby/tests/lighthouse/lighthouse.run.js
+++ gatsby/tests/lighthouse/lighthouse.run.js
@@ -31,7 +31,7 @@ const launchChromeAndRunLighthouse = (
           // Using a "broadband" connection type
           // Corresponds to "Dense 4G 25th percentile" in https://docs.google.com/document/d/1Ft1Bnq9-t4jK5egLSOc28IL4TvR-Tt0se_1faTA4KTY/edit#heading=h.bb7nfy2x9e5v
           rttMs: 40,
-          throughputKbps: 10 * 1024,
+          throughputKbps: 5 * 1024,
           cpuSlowdownMultiplier: 2,
         },
         onlyCategories: ['performance', 'accessibility', 'best-practices', 'seo']
