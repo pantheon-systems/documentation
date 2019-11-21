3bc30e2e73fc5aff48aa6e70243f9b2223ca79c1
Alex Fornuto <alex.fornuto@pantheon.io>
2019-11-21 13:50:05 -0600

    increase latency SKIP_PREVIEW

--- gatsby/tests/lighthouse/lighthouse.run.js
+++ gatsby/tests/lighthouse/lighthouse.run.js
@@ -28,10 +28,8 @@ const launchChromeAndRunLighthouse = (
         emulatedFormFactor: 'mobile',
         skipAudits: ['is-crawlable'],
         throttling: {
-          // Using a "broadband" connection type
-          // Corresponds to "Dense 4G 25th percentile" in https://docs.google.com/document/d/1Ft1Bnq9-t4jK5egLSOc28IL4TvR-Tt0se_1faTA4KTY/edit#heading=h.bb7nfy2x9e5v
-          rttMs: 40,
-          throughputKbps: 5 * 1024,
+          rttMs: 150,
+          throughputKbps: 1 * 1024,
           cpuSlowdownMultiplier: 2,
         },
         onlyCategories: ['performance', 'accessibility', 'best-practices', 'seo']
