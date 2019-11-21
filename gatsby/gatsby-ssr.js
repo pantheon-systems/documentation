3d1d97e1d43dfa5930350bce290d3760d7d5d939
Alex Fornuto <alex.fornuto@pantheon.io>
2019-11-21 16:30:41 -0600

    import atp script

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -7,6 +7,7 @@ export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
     <script key="jquery" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.1/jquery.min.js" />,
     <script key="bootstrap" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" />,
-    <script key="twitter" src="https://platform.twitter.com/widgets.js" />
+    <script key="twitter" src="https://platform.twitter.com/widgets.js" />,
+    <script src="https://pantheon.io/sites/all/themes/zeus/js/atp.min.js?v=1.1" />
   ])
 }
