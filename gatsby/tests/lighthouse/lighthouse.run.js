3c777309fb8386c483a227cb539d94056ef68330
Alex Fornuto <alex.fornuto@pantheon.io>
2019-11-25 14:17:38 -0600

    refactor lighthouse config to separate file SKIP_PREVIEW

--- gatsby/tests/lighthouse/lighthouse.run.js
+++ gatsby/tests/lighthouse/lighthouse.run.js
@@ -2,54 +2,30 @@
 // Node CLI for Lighthouse https://www.npmjs.com/package/lighthouse#using-the-node-cli
 const lighthouse = require("lighthouse");
 
+// Configuration defined in lighthouse-config.js
+const config = require("./lighthouse-config.js")
+
 // Launch Chrome from node
 const chromeLauncher = require("chrome-launcher");
 
+// To interact with the filesystem.
 const fs = require('fs');
 
-const lighthouseConstants = require("./lighthouse-constants");
+// Project Constants
+const lighthouseConstants = require("./lighthouse-constants.js");
 
+// Create the data directory if it doesn't exist.
 if (!fs.existsSync(lighthouseConstants.lighthouseDataDir)) {
   fs.mkdirSync(lighthouseConstants.lighthouseDataDir);
 }
 
+// Import constants from lighthouse-constants.
 const devURL = lighthouseConstants.getDevURL();
 const referenceURL = lighthouseConstants.getReferenceURL();
 
-const DEVTOOLS_RTT_ADJUSTMENT_FACTOR = 3.75;
-const DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR = 0.9;
-const throttling = {
-  DEVTOOLS_RTT_ADJUSTMENT_FACTOR,
-  DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR,
-  mobile3G: {
-    rttMs: 150,
-    throughputKbps: 1.6 * 1024,
-    requestLatencyMs: 150 * DEVTOOLS_RTT_ADJUSTMENT_FACTOR,
-    downloadThroughputKbps: 1.6 * 1024 * DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR,
-    uploadThroughputKbps: 750 * DEVTOOLS_THROUGHPUT_ADJUSTMENT_FACTOR,
-    cpuSlowdownMultiplier: 4,
-  },
-};
-
-const launchChromeAndRunLighthouse = (
-    url,
-    opts = {
-      //chromeFlags: ['--headless'],
-    },
-    config = {
-      extends: 'lighthouse:default',
-      settings: {
-        output: ['html'],
-        emulatedFormFactor: 'mobile',
-        skipAudits: ['is-crawlable'],
-        throttling: throttling.mobile3G,
-        onlyCategories: ['performance', 'accessibility', 'best-practices', 'seo']
-      },
-    }
-  ) =>
-  chromeLauncher.launch({
-    chromeFlags: opts.chromeFlags
-  }).then(chrome => {
+// Define Process of launching Chrome and running Lighthouse
+const launchChromeAndRunLighthouse = ( url, opts = {}, config ) =>
+  chromeLauncher.launch().then(chrome => {
     opts.port = chrome.port;
     return lighthouse(url, opts, config).then(results =>
       chrome.kill().then(() => results)
