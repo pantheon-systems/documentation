84bef82fe2ac4f053fdcdb60f3cdf97cd4b04721
Alex Fornuto <alex.fornuto@pantheon.io>
2019-12-09 17:40:55 -0600

    build docs 404 page

--- gatsby/src/pages/404.js
+++ gatsby/src/pages/404.js
@@ -1,21 +1,36 @@
-import React from "react"
-// import { graphql } from "gatsby"
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import SearchResults from '../components/searchResults';
+import Helmet from "react-helmet"
+import Layout from '../layout/layout';
 
-// import Layout from "../layout/layout"
-// import SEO from "../layout/seo"
+import SEO from "../layout/seo"
+
+
 
 class NotFoundPage extends React.Component {
   render() {
-    // const { data } = this.props
+    const { pathname } = this.props.location
+    const searchPath = pathname.replace(/\//g, ' ').replace(/-/g, ' ')
     // const siteTitle = data.site.siteMetadata.title
-
+    //const { data: { homeYaml, allMdx } } = this.props;
     return (
-      <div>404</div>
-      // <Layout location={this.props.location} title={siteTitle}>
-      //   <SEO title="404: Not Found" />
-      //   <h1>Not Found</h1>
-      //   <p>You just hit a route that doesn&#39;t exist... the sadness...</p>
-      // </Layout>
+      <Layout>
+        <SEO
+          title="404"
+          description="Zoinks! You've hit a URl that doesn't exist. Let's try a search:"
+        />
+        <div style={{ marginTop: '-20px' }} className="container">
+					<div className=" doc-content-well">
+						<div className="">
+							<h1 className="title">404</h1>
+              <h2>Sorry, there's no page at <code>{pathname.replace(/\//g, '')}</code>.</h2>
+              <h3>Wanna <Link to={`/search/?addsearch=${searchPath}`}>Search for {searchPath}</Link>?</h3>
+              <h3>Or go <Link to="/"> Back to all docs</Link>?</h3>
+            </div>
+          </div>
+        </div>
+      </Layout>
     )
   }
 }
