44fa4e99d481a1d8a8ecf14377c894c70014e748
Alex Fornuto <alex.fornuto@pantheon.io>
2019-12-13 12:26:45 -0600

    load atp from CDN

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -7,6 +7,31 @@ export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
     <script key="jquery" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.1/jquery.min.js" />,
     <script key="bootstrap" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" />,
-    <script key="twitter" src="https://platform.twitter.com/widgets.js" />
+    <script key="twitter" src="https://platform.twitter.com/widgets.js" />,
+    <script src="https://d6ddm2cjqbz6v.cloudfront.net/atp.js" async />
+    <script>
+      window.ATP = window.ATP || [];
+      window.ATP.push(function(ATP){
+        ATP.init({
+          debug: true,
+          ctaSelector: '.btn, .cta',
+
+          visibleElements: {
+            'Form': 'form'
+          },
+          nav: {
+            'Header Nav': 'header ul li *',
+            'Footer Nav': 'footer ul li *',
+            'Side Nav': '#sideNav',
+            'Misc Nav': 'nav'
+          },
+          visibleElements: {
+            'Form': 'form',
+            'CTA': '.btn',
+            'Footer': '#footer',
+          },
+        });
+      });
+    </script>
   ])
 }
