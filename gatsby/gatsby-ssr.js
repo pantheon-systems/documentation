f1f21c9f7b19ed99f0f7a1eddcb6dbe956ecb16e
Alex Fornuto <alex.fornuto@pantheon.io>
2019-12-13 12:32:27 -0600

    forgot the comma

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -8,7 +8,7 @@ export const onRenderBody = ({ setPostBodyComponents }) => {
     <script key="jquery" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.1/jquery.min.js" />,
     <script key="bootstrap" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" />,
     <script key="twitter" src="https://platform.twitter.com/widgets.js" />,
-    <script src="https://d6ddm2cjqbz6v.cloudfront.net/atp.js" async />
+    <script src="https://d6ddm2cjqbz6v.cloudfront.net/atp.js" async />,
     <script>
       window.ATP = window.ATP || [];
       window.ATP.push(function(ATP){
