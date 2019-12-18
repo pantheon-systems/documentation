b87e235df15b1609efed751350755c5be7a711f0
Alex Fornuto <alex.fornuto@pantheon.io>
2019-12-18 15:55:51 -0600

    apply cta classes to externalLink when URL matches

--- gatsby/src/components/externalLink.js
+++ gatsby/src/components/externalLink.js
@@ -1,13 +1,22 @@
 import React from "react"
 
 const ExternalLink = ({ text, link }) => {
+  var classes = "external"
+  if (link === "https://pantheon.io/contact-us" ||
+      link === "https://pantheon.io/pantheon-top-edu" ||
+      link === "https://pantheon.io/agencies/partner-program" ||
+      link === "https://pantheon.io/register" )
+    {
+      classes = "external cta docs-cta"
+    }
+
   return (
     <a
       target="_blank"
       rel="nofollow noopener external"
       href={link}
       title={text}
-      className="external"
+      className={classes}
     >
       {text}
     </a>
