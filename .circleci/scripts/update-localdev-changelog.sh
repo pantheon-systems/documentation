24fe172adfcacee13b33e5fb342a29297069f6af
Alex Fornuto <alex.fornuto@pantheon.io>
2020-01-14 10:11:22 -0600

    update cache and echo

--- .circleci/scripts/update-localdev-changelog.sh
+++ .circleci/scripts/update-localdev-changelog.sh
@@ -8,5 +8,5 @@ cd $BUILD_PATH
 echo "Downloading Localdev changelog..."
 wget -O ./source/data/localdev/changelog.yml https://pantheon-localdev.s3.amazonaws.com/changelog.yml
 
-echo "Apply md//"
+echo "Apply markdown callout"
 sed -i -e 's/|/!markdown |/g' source/data/localdev/changelog.yml
