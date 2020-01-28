d3abb3e4bda2b222ebb69ecaf9ee75f38a64b5e3
Alex Fornuto <alex.fornuto@pantheon.io>
2020-01-28 17:08:07 -0600

    linkify commit references

--- .circleci/scripts/update-drush-changelog.sh
+++ .circleci/scripts/update-drush-changelog.sh
@@ -6,7 +6,10 @@ set -e
 cd $BUILD_PATH
 
 echo "Downloading Localdev changelog..."
-curl https://api.github.com/repos/drush-ops/drush/releases -o ./source/data/drush/drush-changelog.json
+curl https://api.github.com/repos/drush-ops/drush/releases -o ./source/data/drush.json
 
-echo "Convert local GH references to Markdown links"
+echo "Convert local Issue and Pull references to Markdown links"
 sed -i -E "s/(#)([0-9][0-9][0-9][0-9])/\[#\2\]\(https:\/\/github.com\/drush-ops\/drush\/issues\/\2)/g" ./source/data/drush.json
+sleep 1
+echo "Convert local commit refs to links"
+sed -i -E "s/(body\"\:.*)([a-fA-F0-9]{7})\ /\1\[\2\]\(https:\/\/github.com\/drush-ops\/drush\/commit\/\2\)/g" ./source/data/drush.json
