fb8e5d22e3288fb53e6806a7e6588b48fe3c83bc
Alex Fornuto <alex@fornuto.com>
2020-04-16 11:36:15 -0500

    convert to component

--- src/pages/dns-providers.js
+++ src/pages/dns-providers.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../layout/layout"
 import SEO from "../layout/seo"
+import DNSProviderDocs from "../components/dns-provider-docs"
 
 {
   /* @TODO Convert to a React Component */
@@ -14,9 +15,6 @@ const previewFlexPanelItem = {
 
 class dnsProviders extends React.Component {
   render() {
-    const data = this.props.data.allMdx
-    //console.log("data: ", data) // FOR DEBUGGING
-    //console.log ("data.nodes: ", data.nodes) //DEBUGGING
     return (
       <>
         <SEO title="DNS Providers" />
@@ -27,22 +25,7 @@ class dnsProviders extends React.Component {
                 <h1 className="title">DNS Providers</h1>
               </div>
               <div className="row mb-70">
-                <ul>
-                  {data.nodes.map((page, i) => {
-                    return (
-                      <>
-                        <li key={`index-${i}`}>
-                          <Link
-                            to={page.fields.slug}
-                            title={page.frontmatter.provider}
-                          >
-                            {page.frontmatter.provider}
-                          </Link>
-                        </li>
-                      </>
-                    )
-                  })}
-                </ul>
+                <DNSProviderDocs />
               </div>
             </div>
           </div>
@@ -53,21 +36,3 @@ class dnsProviders extends React.Component {
 }
 
 export default dnsProviders
-
-export const pageQuery = graphql`
-  {
-    allMdx(
-      filter: { fileInfo: { absolutePath: { regex: "/.dns-providers./" } } }
-      sort: { fields: frontmatter___title, order: ASC }
-    ) {
-      nodes {
-        fields {
-          slug
-        }
-        frontmatter {
-          provider
-        }
-      }
-    }
-  }
-`
