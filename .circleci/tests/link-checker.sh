c50c95688aac496116ce84208a778ddc2af70d48
Alex Fornuto <alex@fornuto.com>
2020-04-21 18:00:50 -0500

    update and implement link tests

--- .circleci/tests/link-checker.sh
+++ .circleci/tests/link-checker.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
+
+# This script takes as an argument a target URL to test, then checks recursively for broken (relative) links.
+
 set -e
 
-# This script assumes you're already serving the site using --prefix-paths
+# Exits if no target is provided as an argument.
+if [ "$0" = ""]; then
+  echo "You forgot to provide a target."
+  exit 1
+fi
 
 echo "Checking for broken links.."
 
-cd $BUILD_PATH
-
-if ./node_modules/broken-link-checker/bin/blc -q -r -e -o http://localhost:9000/docs
+if ./node_modules/broken-link-checker/bin/blc -q -r -e -o $0
 then
   echo "No broken internal links!"
 else
