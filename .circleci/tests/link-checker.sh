c71d71b0824564b90e29e28eb991e970be63a08d
Alex Fornuto <alex@fornuto.com>
2020-04-22 10:14:51 -0500

    update link checker

--- .circleci/tests/link-checker.sh
+++ .circleci/tests/link-checker.sh
@@ -5,17 +5,16 @@
 set -e
 
 # Exits if no target is provided as an argument.
-if [ "$0" = ""]; then
+if [ "$1" = "" ]; then
   echo "You forgot to provide a target."
   exit 1
 fi
 
-echo "Checking for broken links.."
+echo "Checking for broken links on ${1} .."
 
-if ./node_modules/broken-link-checker/bin/blc -q -r -e -o $0
+if ./node_modules/broken-link-checker/bin/blc -q -r -e -o http://$1
 then
   echo "No broken internal links!"
 else
   exit 1
 fi
-
