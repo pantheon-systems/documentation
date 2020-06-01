ad0ecd68bbf4f97e3decaa800555328c7ccf4598
Alex Fornuto <alex@fornuto.com>
2020-06-01 14:50:35 -0500

    increase GetFeedback iframe height

--- src/components/getFeedback.js
+++ src/components/getFeedback.js
@@ -4,7 +4,7 @@ const GetFeedBack = ({ formId, page, topic }) => {
   return (
     <iframe
       title="GetFeedBack"
-      style={{ width: "100%", minHeight: "300px" }}
+      style={{ width: "100%", minHeight: "320px" }}
       frameBorder="0"
       src={`https://www.getfeedback.com/r/${formId}?page=${page}&topic=${topic}`}
     />
