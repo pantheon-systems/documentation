4dab6626d7e3b52b70848d756ecd6e08cf4dbeeb
Alex Fornuto <alex@fornuto.com>
2020-07-10 11:07:49 -0500

    use try3 function to increase link check resliency

--- .circleci/tests/link-checker.sh
+++ .circleci/tests/link-checker.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
-
 # This script takes as an argument a target URL to test, then checks recursively for broken (relative) links.
-
 set -e
+
+# Import Functions
+source $BUILD_PATH/.circleci/scripts/functions.sh
 
 # Exits if no target is provided as an argument.
 if [ "$1" = "" ]; then
@@ -12,7 +13,7 @@ fi
 
 echo "Checking for broken links on ${1} .."
 
-if blc -q -r -e -o $1
+if try3 blc -q -r -e -o $1
 then
   echo "No broken internal links!"
 else
