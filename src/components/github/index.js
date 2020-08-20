1d224938ae933c128c22e741a1501222d99f4c22
Alex Fornuto <alex@fornuto.com>
2020-08-20 16:22:57 -0500

    more css cleanup
    I should probably squash all this.

--- src/components/github/index.js
+++ src/components/github/index.js
@@ -7,7 +7,6 @@ const Github = ({ pageTitle, path, editPath }) => {
       <a
         href={`https://github.com/pantheon-systems/documentation/edit/main/${editPath}`}
         target="blank"
-        style={{ color: "black" }}
       >
         <i className="fa fa-github" /> Edit this page on GitHub
       </a>
@@ -15,7 +14,6 @@ const Github = ({ pageTitle, path, editPath }) => {
       <a
         href={`https://github.com/pantheon-systems/documentation/issues/new?title=${pageTitle}%20Doc%20Update%20&body=Re%3A%20%5B${pageTitle}%5D(https%3A%2F%2Fpantheon.io/docs/${path})%0A%0APriority%3A%20Low%2FMedium%2FHigh%20(choose%20one%2C%20remove%20the%20other%20options)%0A%0A%23%23%20Issue%20Description%3A%0A%0A%23%23%20Suggested%20Resolution`}
         target="blank"
-        style={{ color: "black" }}
       >
         <i className="fa fa-github" /> Report an issue with this doc
       </a>
