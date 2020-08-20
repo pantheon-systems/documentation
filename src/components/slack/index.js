e0220c4d64bdd69b0de7cdb476e1e4c96a57d8e9
Alex Fornuto <alex@fornuto.com>
2020-08-20 14:37:57 -0500

    gradients, copy, cleanup

--- src/components/slack/index.js
+++ src/components/slack/index.js
@@ -1,26 +1,3 @@
-/*
-import React from "react"
-
-const Slack = () => {
-  return (
-    <iframe
-      title="Slack"
-      src="https://slackin.pantheon.io/iframe"
-      className="__slackin"
-      style={{
-        borderWidth: 0,
-        width: "200px",
-        height: "20px",
-        visibility: "visible",
-        marginLeft: 3,
-      }}
-    />
-  )
-}
-
-export default Slack
-*/
-
 import React from "react"
 import './style.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -35,7 +12,7 @@ const Slack = () => {
         <a 
         className="btn btn-slack fa"
         href="https://slackin.pantheon.io/"
-      > <FontAwesomeIcon icon={faSlack} size="1x" className="fa" /> Slack</a>
+      > <FontAwesomeIcon icon={faSlack} size="1x" className="fa" /> Discuss in Slack</a>
   )
 }
 
