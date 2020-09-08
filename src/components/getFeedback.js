4ebbce99cd357d4a8b59ffcefc836debdc68664c
Alex Fornuto <alex@fornuto.com>
2020-09-08 15:39:48 -0500

    rm window call

--- src/components/getFeedback.js
+++ src/components/getFeedback.js
@@ -6,7 +6,7 @@ const GetFeedBack = ({ formId, page, topic }) => {
     {(function(w,d,s,u,g,a,b){w['GetFeedbackObject']=g;w[g]=w[g]||function(){
       (w[g].q=w[g].q||[]).push(arguments)};a=d.createElement(s),
       b=d.getElementsByTagName(s)[0];a.async=1;a.src=u;b.parentNode.insertBefore(a,b)
-    })(window,document,'script','//websites.cdn.getfeedback.com/embed/4et5x7k3g8/gf.js','gf')}
+    })(document,'script','//websites.cdn.getfeedback.com/embed/4et5x7k3g8/gf.js','gf')}
   </script>
 
   )
