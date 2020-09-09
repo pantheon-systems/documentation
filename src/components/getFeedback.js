7d1d0f7471abf55d04848c754d83b98f4fdfcdf0
Alex Fornuto <alex@fornuto.com>
2020-09-09 11:49:34 -0500

    import window and document from package global

--- src/components/getFeedback.js
+++ src/components/getFeedback.js
@@ -1,20 +1,27 @@
 import React from "react"
+import window from "global/window"
+import document from "global/document"
 
 const GetFeedBack = ({ formId, page, topic }) => {
 
   return (
     <>
     <script dangerouslySetInnerHTML={{
-      __html: 
+      __html:` 
           (function(w,d,s,u,g,a,b){w['GetFeedbackObject']=g;w[g]=w[g]||function(){
           (w[g].q=w[g].q||[]).push(arguments)};a=d.createElement(s),
           b=d.getElementsByTagName(s)[0];a.async=1;a.src=u;b.parentNode.insertBefore(a,b)
-          })(window,document,'script','//websites.cdn.getfeedback.com/embed/4et5x7k3g8/gf.js','gf')
-          (gf('setParameters', {'PAGE': page, 'TOPIC': topic}))
+          })(window,document,'script','//websites.cdn.getfeedback.com/embed/4et5x7k3g8/gf.js','gf')`
         ,
       }}
     />
 
+    <script dangerouslySetInnerHTML={{
+      __html:`
+        (gf('setParameters', {'PAGE': ${page}, 'TOPIC': ${topic}}))
+      `
+    }}
+    />
     </>
   )
 }
