b8dec0aaac9f398d6040b27c5f2af15a9eaed452
Alex Fornuto <alex@fornuto.com>
2021-01-15 14:53:39 -0600

    init new report page, document function

---
+++ src/reports/unusedImages.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react"
+import { StaticQuery, graphql, Link } from "gatsby"
+import Layout from "../layout/layout"
+//import newGitHubIssueUrl from "new-github-issue-url"
+//import Image from "../layout/image"
+
+
+/* This report creates a table of every image in /source/images which isn't being used by a piece of content. */
+
+//Create a data object of all images in /source/images
+
+
+//Create a data object of all content (docs, guides, partials)
+
+//Create a function which builds an array of image paths where the path is not found in any content piece.
+
+// Create a factory function which returns table rows for each instance in the aforementioned array.
+
+// Create the page.
