f1ac95bc27850b0d5062c77b96800c0d92cbed33
Alex Fornuto <github@alexf.co>
2021-02-02 12:26:43 -0600

    Revert "Purge Unused Images & Report"

--- src/layout/topics-grid/index.js
+++ src/layout/topics-grid/index.js
@@ -1,20 +1,15 @@
-import React from "react"
-import PropTypes from "prop-types"
-import TopicItem from "../topic-item"
+import React from 'react';
+import PropTypes from 'prop-types';
+import TopicItem from '../topic-item';
 const propTypes = {
-  topics: PropTypes.array,
-}
+	topics: PropTypes.array
+};
 
 function TopicsGrid(props) {
-  const { topics } = props
-  return (
-    <div className="topics-grid flex-panel-group">
-      {topics &&
-        topics.map(topic => <TopicItem topic={topic} key={topic.title} />)}
-    </div>
-  )
+	const { topics } = props;
+	return <div className="topics-grid flex-panel-group">{topics && topics.map((topic) => <TopicItem topic={topic} key={topic.title} />)}</div>;
 }
 
-TopicsGrid.propTypes = propTypes
+TopicsGrid.propTypes = propTypes;
 
-export default TopicsGrid
+export default TopicsGrid;
