b8e9821bea88e92b792fb0f1378b140efdeb2db3
Alex Fornuto <alex@fornuto.com>
2021-02-17 15:48:54 -0600

    comment 404 logic

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,16 +8,17 @@ import SVG404 from "../../source/images/404_dark.svg"
 
 class NotFoundPage extends React.Component {
 
-componentDidMount() {
+componentDidMount() { //On page load...
+    const { pathname } = this.props.location // pull the "location" prop and save it as 'pathname'
+    var searchPath = pathname.replace(/\//g, "").replace(/-/g, " ") // define searchPath as pathname by removing the slash and replacing dashes w with spaces
+    var searchPath = searchPath.replace("docs", "")// Remove docs from the search path
+    window.location.href.toString().includes("addsearch") // If the current page address includes "addsearch"...
+      ? null // Do nothing
+      : (window.location = ` 404?addsearch=${searchPath}`) // Otherwise, rewrite the URL (which I think inits a reload) to form the UTM parameters Addsearch needs to run a query.
+      // Note: This while mess is here not only because of my novice React skills, but because Addsearch would not accept search parameters by any form other than UTM parameters.
+      // An Algolia search solution will likely render most of this as removable.
 
-    const { pathname } = this.props.location
-    var searchPath = pathname.replace(/\//g, "").replace(/-/g, " ")
-    var searchPath = searchPath.replace("docs", "")
-    window.location.href.toString().includes("addsearch")
-      ? null
-      : (window.location = ` 404?addsearch=${searchPath}`)
-
-    window.addsearch_settings = {
+    window.addsearch_settings = { // These are a bunch of key/value pairs addsearch wants. So we set them. See Addsearch's "documentation" for more info.
       display_url: true,
       display_resultscount: false,
       display_sortby: false,
@@ -26,7 +27,7 @@ componentDidMount() {
       number_of_results: 4,
     }
 
-    const script = document.createElement("script")
+    const script = document.createElement("script") // Loads the Addsearch JS blob from them
     script.setAttribute(
       "src",
       `https://addsearch.com/js/?key=a7b957b7a8f57f4cc544c54f289611c6&type=resultpage`
@@ -46,7 +47,7 @@ componentDidMount() {
       <Layout type="404">
         <SEO
           title="404"
-          description="Zoinks! You've hit a URl that doesn't exist. Let's try a search:"
+          description="Zoinks! You've hit a URL that doesn't exist. Let's try a search:"
         />
         <div style={{ marginTop: "-20px" }} className="container">
           <main className=" doc-content-well" id="docs-main">
