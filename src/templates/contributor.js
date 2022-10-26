ffac26575588c8b5651409ea56683f1f3a101c03
Edward Angert <EdwardAngert@users.noreply.github.com>
2022-10-26 11:52:10 -0400

    add overview else to contrib (#7843)
    * add overview else to contrib
    
    * curly bracket
    
    * let printedOverview

--- src/templates/contributor.js
+++ src/templates/contributor.js
@@ -37,6 +37,7 @@ class ContributorTemplate extends React.Component {
     const docs =
       this.props.data.allDocs != null ? this.props.data.allDocs.edges : []
     let printedGuides = []
+    let printedOverview = []
     return (
       <Layout>
         <SEO title={contributor.name} />
@@ -89,6 +90,19 @@ class ContributorTemplate extends React.Component {
                           )
                         }
                       } else {
+                        if (/^\/overview\/.*$/.test(node.fields.slug)) {
+                        const result = /^(\/overview\/[A-Za-z0-9\-\_]+)\/?.*$/.exec(node.fields.slug)
+                        // Use printedOverview to avoid showing the same guide twice.
+                        if (printedOverview.indexOf(result[1]) === -1) {
+                          printedOverview.push(result[1])
+                          return (
+                            <li key={node.id}>
+                              <Link to={`${result[1]}`}>
+                                {node.frontmatter.title}
+                              </Link>
+                            </li>
+                          )
+                        }
                         return (
                           <li key={node.id}>
                             <Link to={`/${node.fields.slug}`}>
@@ -96,7 +110,7 @@ class ContributorTemplate extends React.Component {
                             </Link>
                           </li>
                         )
-                      }
+                      }}
                     })}
                   </ul>
                 </div>
