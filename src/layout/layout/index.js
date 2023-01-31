f0897af27bf7fa90c4e958621edd6c16b4bacaa7
Rosa Isela Lopez <rosaislp@gmail.com>
2023-01-31 10:22:09 -0600

    add InstantSearch as wrapper of Layout to access to the algolia context from children

--- src/layout/layout/index.js
+++ src/layout/layout/index.js
@@ -1,16 +1,31 @@
-import React, { Components} from "react"
+import React from "react"
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch } from 'react-instantsearch-dom';
+import config from '../../algolia/config';
 import Header from "../header"
 import Footer from "../footer"
 import './style.css'
 
+const searchClient = algoliasearch(
+  config.search.algoliaAppId,
+  config.search.algoliaSearchKey
+);
+
 const Layout = (props) => {
   const pageType = props.type ? props.type : "default"
+
   return (
-    <div className="pantheon-docs">
-      <Header page={pageType}/>
-      {props.children}
-      <Footer />
-    </div>
+    <InstantSearch
+      searchClient={searchClient}
+      indexName={config.search.indexName}
+      onSearchStateChange={() => {}}
+    >
+      <div className="pantheon-docs">
+        <Header page={pageType} />
+        {props.children}
+        <Footer />
+      </div>
+    </InstantSearch>
   )
 }
 
