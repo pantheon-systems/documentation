9ec2845adfc9f7c999a7b2bcefca678ef3a5bb0b
edwardangert <EdwardAngert@users.noreply.github.com>
2023-02-17 10:51:53 -0500

    revert algolia to addsearch

--- src/layout/layout/index.js
+++ src/layout/layout/index.js
@@ -1,30 +1,16 @@
-import React from "react"
-import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch } from 'react-instantsearch-dom';
-import config from '../../algolia/config';
+import React, { Components} from "react"
 import Header from "../header"
 import Footer from "../footer"
 import './style.css'
 
-const searchClient = algoliasearch(
-  config.search.algoliaAppId,
-  config.search.algoliaSearchKey
-);
-
 const Layout = (props) => {
   const pageType = props.type ? props.type : "default"
-
   return (
-    <InstantSearch
-      searchClient={searchClient}
-      indexName={config.search.indexName}
-    >
-      <div className="pantheon-docs">
-        <Header page={pageType} />
-        {props.children}
-        <Footer />
-      </div>
-    </InstantSearch>
+    <div className="pantheon-docs">
+      <Header page={pageType}/>
+      {props.children}
+      <Footer />
+    </div>
   )
 }
 
