baa60b48ff9405a8f498e9f6595e9cd14b337569
edwardangert <EdwardAngert@users.noreply.github.com>
2023-02-23 15:24:29 -0500

    prod onetrust

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,7 +9,7 @@ export const onRenderBody = ({ setPostBodyComponents }) => {
     <script key="bootstrap" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" />,
     <script key="twitter" src="https://platform.twitter.com/widgets.js" />,
     // OneTrust Banner
-    <script src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js" type="text/javascript" charset="UTF-8" data-domain-script="fba5027b-04c0-4165-8778-4e10fb9f5fa3-test" />,
+    <script src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js" type="text/javascript" charset="UTF-8" data-domain-script="fba5027b-04c0-4165-8778-4e10fb9f5fa3" />,
       function OptanonWrapper() {
         window.dataLayer.push({'event':'OneTrustGroupsUpdated'})
       }
