57155b5af28864af881ff271ffb1bf9639538a2f
wordsmither <jennifer.hoffman@pantheon.io>
2023-03-17 13:34:30 -0700

    it works

--- src/reports/schema.js
+++ src/reports/schema.js
@@ -5,82 +5,86 @@ import showdown from "showdown"
 
 const converter = new showdown.Converter()
 
-
 class CategoryTree extends React.Component {
-    render() {
-        return (
-            <StaticQuery
-            query={graphql`
-            query {
-                allSchemaYaml {
-                    edges {
-                        node {
-                            tag
-                            description
-                            type
-                            required
-                            multi
-                            valid_values {
-                                group
-                                values
-                            }
-                        }
-                    }
+  render() {
+    return (
+      <StaticQuery
+        query={graphql`
+          query {
+            allSchemaYaml {
+              edges {
+                node {
+                  tag
+                  description
+                  type
+                  required
+                  multi
+                  valid_values {
+                    group
+                    values
+                  }
                 }
+              }
             }
-            `}
-            render={data => {
-                const yamlfile = data.allSchemaYaml.edges
-                
-                return (
-                    <Layout>
-                    <h1>Schema</h1>
-                    <div style={{ padding: "20px" }}>
-                    {yamlfile.map((tag, i) => {
-                        return(
-                            <h2 key={i}>
-                            {tag.node.tag}
-                            <p>{tag.node.description}</p>
-                            <ul>
-                            <li>Type: {tag.node.type}</li>
-                            <li>Required: {tag.node.required}</li>
-                            <li>Multiple values allowed: {tag.node.multiple}</li>
-                            <li>Valid Values:
-                            <ul>
-                            {tag.node.valid_values.map((groups, i) => {
-                                return (
-                                    <li key={i}>
-                                    {groups.group}
-                                    <ul>
+          }
+        `}
+        render={(data) => {
+          const yamlfile = data.allSchemaYaml.edges
+
+          return (
+            <Layout>
+              <h1>Schema</h1>
+              <div style={{ padding: "20px" }}>
+                {yamlfile.map((tag, i) => {
+                  return (
+                    <h2 key={i}>
+                      {tag.node.tag}
+                      <p>{tag.node.description}</p>
+                      <ul>
+                        <li>Type: {tag.node.type}</li>
+                        <li>Required: {tag.node.required}</li>
+                        <li>Multiple values allowed: {tag.node.multiple}</li>
+                        <li>
+                          Valid Values:
+                          <ul>
+                          {tag.node.valid_values.map((groups, i) => {
+                              if (groups.group === "None")
+                              return (
+                                  <div>
                                     {groups.values.map((value, i) => {
-                                        return (
-                                            <li key={i}>
-                                            {value}
-                                            </li>
-                                            )
-                                        })}
-                                        </ul>
-                                        </li>
-                                        )
-                                    })
-                                }
-                                </ul>
+                                      return <li key={i}>{value}</li>
+                                    })}
+                                  </div>
+                              )
+                              if (groups.group !== "None")
+                              return (
+                                <li key={i}>
+                                  {groups.group}
+                                  <ul>
+                                    {groups.values.map((value, i) => {
+                                      return <li key={i}>{value}</li>
+                                    })}
+                                  </ul>
                                 </li>
-                                </ul>
-                                </h2>
-                                )
+                              )
                             })}
-                            </div>
-                            </Layout>
-                            )
-                        }}
-                        />
-                        )
-                    }
-                }
-                
-                export default CategoryTree
-                
-                /* .filter(doc => {
+                          </ul>
+                        </li>
+                      </ul>
+                    </h2>
+                  )
+                })}
+              </div>
+            </Layout>
+          )
+        }}
+      />
+    )
+  }
+}
+
+export default CategoryTree
+
+/* .filter(doc => {
                     return doc.title.indexOf(search) >= 0
                 })*/
