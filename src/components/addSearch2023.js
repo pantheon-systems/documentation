4917139acf58bb16b978122578529f59514e9a98
Steve Persch <stevepersch@steves-laptop.lan>
2023-08-23 17:04:59 -0500

    in-progress addsearch fix

---
+++ src/components/addSearch2023.js
@@ -0,0 +1,42 @@
+import React from "react"
+
+
+class AddSearch extends React.Component {
+  componentDidMount() {
+    const resultPage = "/hi-rachel"
+    window.addsearch_settings = {
+      "search_widget": {
+        "placeholder": "Search Pantheon Docs",
+        "show_search_suggestions": true,
+        "search_suggestion_position": "left",
+        "default_sortby": "relevance",
+        "display_date": false,
+        "display_meta_description": true,
+        "display_result_image": false,
+        "link_target": "_blank",
+        "hide_logo": false,
+        "direction": "ltr",
+        "api_throttle_time": 2000,
+        "automatic_filter_results_by_site_language": false,
+        "analytics_enabled": true
+      }
+    }
+    const script = document.createElement("script")
+ //alert(resultPage);
+
+    script.setAttribute(
+      "src",
+      `https://cdn.addsearch.com/v5/addsearch-ui.min.js?key=a7b957b7a8f57f4cc544c54f289611c6&id=search_widget${
+        resultPage.includes("hello") ? "&type=resultpage" : ""
+      }`
+    )
+    script.setAttribute("defer", true)
+
+    document.body.appendChild(script)
+  }
+  render() {
+    return <div className="addsearch-container" />
+  }
+}
+
+export default AddSearch
