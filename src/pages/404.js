900f19fd1ec753027104cb0da88cca9a234b686b
Rachel Whitton <rachel@pantheon.io>
2023-08-31 08:33:27 -0500

    Remove stale component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-import SearchResults from "../components/searchResults"
 import Helmet from "react-helmet"
 import Layout from "../layout/layout"
 import SEO from "../layout/seo"
