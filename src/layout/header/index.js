7138ada7cf6131d9887613f99b9ad4decee38268
M Miller <melissa.miller@pantheon.io>
2023-08-31 11:42:56 -0700

    Header WIP snapshot

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -2,11 +2,46 @@ import React from "react"
 import { StaticQuery, graphql, Link } from "gatsby"
 
 import AddSearch from "../../components/addSearch"
-import { Navbar } from "@pantheon-systems/pds-toolkit-react"
+import { MenuButton, Navbar } from "@pantheon-systems/pds-toolkit-react"
 
 import "./style.css"
 
+const documentationMenuItems = [
+  {
+    isNode: true,
+    nodeContent: (
+      <Link id="get-started" to="/get-started">
+        Get Started
+      </Link>
+    ),
+  },
+]
+
 const navbarChildren = [
+  <MenuButton
+    key="1"
+    slot="items-left"
+    label="Documentation"
+    variant="navbar"
+    withinNavbar
+    menuItems={documentationMenuItems}
+  />,
+  <Link
+    key="2"
+    slot="items-left"
+    className="pds-button pds-button--navbar"
+    to="/changelog"
+  >
+    Changelog
+  </Link>,
+  <Link
+    key="2"
+    slot="items-left"
+    className="pds-button pds-button--navbar"
+    to="/glossary"
+  >
+    Glossary
+  </Link>,
   <a
     key="2"
     slot="items-right"
