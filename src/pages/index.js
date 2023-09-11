247ebbc9756cc7943f9c4e6e373c7ba19fa186db
M Miller <melissa.miller@pantheon.io>
2023-09-11 14:02:28 -0700

    Begin organizing home page markup

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,41 +20,34 @@ class Index extends React.Component {
           description="Information for building, launching, and running dynamic sites on the Pantheon Website Management Platform"
           image={"/images/assets/default-thumb-doc.png"}
         />
-          <main className="container doc-content-well" id="docs-main">
-            <div className="row">
-              <h1 className="title col-sm-12">{homeYaml.title}</h1>
-            </div>
-            <div className="row" style={{ marginBottom: "15px" }}>
-              <div className="col-md-12">
-                <CallToAction
-                  title={homeYaml.call_to_action.title}
-                  subTitle={homeYaml.call_to_action.sub_title}
-                  url={homeYaml.call_to_action.url}
-                  type={homeYaml.call_to_action.type}
-                />
-              </div>
-            </div>
-            <div className="row mb-70">
-              <div className="col-md-12">
-                <TopicsGrid topics={homeYaml.topics} />
-              </div>
-            </div>
-            <ThreeColumnList
-              title={homeYaml.three_column_links.title}
-              links={homeYaml.three_column_links.links}
+        <main className="pds-container pds-container--wide" id="docs-main">
+          <h1>{homeYaml.title}</h1>
+
+          <CallToAction
+            title={homeYaml.call_to_action.title}
+            subTitle={homeYaml.call_to_action.sub_title}
+            url={homeYaml.call_to_action.url}
+            type={homeYaml.call_to_action.type}
+          />
+
+          <TopicsGrid topics={homeYaml.topics} />
+
+          <ThreeColumnList
+            title={homeYaml.three_column_links.title}
+            links={homeYaml.three_column_links.links}
+          />
+          <ChangelogPreview
+            title={homeYaml.changelog_preview.title}
+            url={homeYaml.changelog_preview.url}
+            changelogs={allMdx.edges}
+          />
+          <Helmet title="Pantheon Docs" titleTemplate={`%s`}>
+            <meta
+              name="msvalidate.01"
+              content="9A4A4920CF12D3347BFF74AD7E92D154"
             />
-            <ChangelogPreview
-              title={homeYaml.changelog_preview.title}
-              url={homeYaml.changelog_preview.url}
-              changelogs={allMdx.edges}
-            />
-            <Helmet title="Pantheon Docs" titleTemplate={`%s`}>
-              <meta
-                name="msvalidate.01"
-                content="9A4A4920CF12D3347BFF74AD7E92D154"
-              />
-            </Helmet>
-          </main>
+          </Helmet>
+        </main>
       </Layout>
     )
   }
@@ -93,9 +86,9 @@ export const pageQuery = graphql`
     }
 
     allMdx(
-      filter: { 
-        fileAbsolutePath: { regex: "/changelogs/" } 
-        frontmatter: { draft: {ne: true}}
+      filter: {
+        fileAbsolutePath: { regex: "/changelogs/" }
+        frontmatter: { draft: { ne: true } }
       }
       sort: { fields: [fileAbsolutePath], order: DESC }
       limit: 4
