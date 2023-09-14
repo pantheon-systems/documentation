720c49645911cfd5bce2d3d5d079a107dd8cea4c
M Miller <melissa.miller@pantheon.io>
2023-09-14 14:51:36 -0700

    Update 404.js
    Rough design for now

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,27 +3,29 @@ import Layout from "../layout/layout"
 import SEO from "../layout/seo"
 import SVG404 from "../../source/images/404_dark.svg"
 
+import { Container, FlexContainer } from "@pantheon-systems/pds-toolkit-react"
+
 class NotFoundPage extends React.Component {
+  componentDidMount() {
+    //On page load...
 
-componentDidMount() { //On page load...
-
-      window.addsearch_settings = {
-        "search_widget": {
-          "placeholder": "Search Pantheon Docs",
-          "show_search_suggestions": true,
-          "search_suggestion_position": "left",
-          "default_sortby": "relevance",
-          "display_date": false,
-          "display_meta_description": true,
-          "display_result_image": false,
-          "link_target": "_blank",
-          "hide_logo": false,
-          "direction": "ltr",
-          "api_throttle_time": 2000,
-          "automatic_filter_results_by_site_language": false,
-          "analytics_enabled": true
-        }
-      }
+    window.addsearch_settings = {
+      search_widget: {
+        placeholder: "Search Pantheon Docs",
+        show_search_suggestions: true,
+        search_suggestion_position: "left",
+        default_sortby: "relevance",
+        display_date: false,
+        display_meta_description: true,
+        display_result_image: false,
+        link_target: "_blank",
+        hide_logo: false,
+        direction: "ltr",
+        api_throttle_time: 2000,
+        automatic_filter_results_by_site_language: false,
+        analytics_enabled: true,
+      },
+    }
 
     const script = document.createElement("script") // Loads the Addsearch JS blob from them
     script.setAttribute(
@@ -44,16 +46,24 @@ componentDidMount() { //On page load...
           title="404"
           description="Zoinks! You've hit a URL that doesn't exist. Let's try a search:"
         />
-        <div style={{ marginTop: "-20px" }} className="container">
+        <Container width="narrow">
           <main className=" doc-content-well" id="docs-main">
-            <div className="mb-70">
-              <img className="notfound" src={SVG404} />
-
-              <h2>Sorry, there's no page at that URL.</h2>
-
-            </div>
+            <FlexContainer
+              alignContent="start"
+              alignItems="center"
+              flexDirection="column"
+              mobileFlex="same"
+              className="pds-spacing-mar-block-end-6xl"
+            >
+              <h1>Sorry, there's no page at that URL.</h1>
+              <img
+                className="notfound"
+                style={{ maxWidth: 400 }}
+                src={SVG404}
+              />
+            </FlexContainer>
           </main>
-        </div>
+        </Container>
       </Layout>
     )
     return <div className="addsearch-container" />
