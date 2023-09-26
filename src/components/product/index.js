a6e2275cebf6c16edc9833ca98af6b4f6bf81b9d
M Miller <melissa.miller@pantheon.io>
2023-09-25 17:45:01 -0700

    Theme product cards

--- src/components/product/index.js
+++ src/components/product/index.js
@@ -1,16 +1,24 @@
 import React from "react"
-import './style.css';
+
+import { Icon } from "@pantheon-systems/pds-toolkit-react"
+
+import "./style.css"
 
 const Product = ({ title, link, children }) => {
   return (
-    <div className="flex-panel-item">
-      <div className="flex-panel-body">
-        <div className="flex-panel-title">
-          <h3 className="plugin-title">{title}</h3>
-        </div>
-        <p className="topic-info__description">{children}</p>
-        <a href={link} className="btn-primary btn get-plugin">
+    <div className="product-card pds-grid-item pds-grid-item--sm-4 pds-grid-item--md-6 pds-grid-item--lg-4">
+      <h3 className="product-card__title">{title}</h3>
+
+      <div className="product-card__text">{children}</div>
+
+      <div className="product-card__link">
+        <a href={link} target="_blank" className="pds-button">
           Learn More
+          <Icon
+            iconName="arrowRight"
+            iconSize="md"
+            className="pds-spacing-pad-inline-start-5xs"
+          />
         </a>
       </div>
     </div>
