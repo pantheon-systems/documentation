2b116d90d94c22f75bb33ea3eac33696ff25c828
Rachel Whitton <rachel@pantheon.io>
2023-09-27 16:42:03 -0500

    Fix separate search results page

--- src/pages/search.js
+++ src/pages/search.js
@@ -5,36 +5,66 @@ import SEO from "../layout/seo"
 import { Container } from "@pantheon-systems/pds-toolkit-react"
 
 class Search extends React.Component {
-  componentDidMount() {
-    //On page load...
-
-    window.addsearch_settings = {
-      search_widget: {
-        placeholder: "Search Pantheon Docs",
-        show_search_suggestions: true,
-        search_suggestion_position: "left",
-        default_sortby: "relevance",
-        display_date: false,
-        display_meta_description: true,
-        display_result_image: false,
-        link_target: "_blank",
-        hide_logo: false,
-        direction: "ltr",
-        api_throttle_time: 2000,
-        automatic_filter_results_by_site_language: false,
-        analytics_enabled: true,
-      },
-    }
-
-    const script = document.createElement("script") // Loads the Addsearch JS blob from them
-    script.setAttribute(
+  componentDidMount() { //On page load...
+    const addsearch_js_client = document.createElement("script") // Loads the Addsearch JS blob from them
+    addsearch_js_client.setAttribute(
       "src",
-      `https://cdn.addsearch.com/v5/addsearch-ui.min.js?key=a7b957b7a8f57f4cc544c54f289611c6&id=search_widget`
+      `https://cdn.jsdelivr.net/npm/addsearch-js-client@0.8/dist/addsearch-js-client.min.js`
     )
-    script.setAttribute("defer", true)
+    addsearch_js_client.setAttribute("defer", true)
+    document.body.appendChild(addsearch_js_client)
 
-    document.body.appendChild(script)
-  }
+
+    const addsearch_search_ui = document.createElement("script") // Loads the Addsearch JS blob from them
+    addsearch_search_ui.setAttribute(
+      "src",
+      `https://cdn.jsdelivr.net/npm/addsearch-search-ui@0.7/dist/addsearch-search-ui.min.js`
+    )
+    addsearch_search_ui.setAttribute("defer", true)
+    addsearch_search_ui.onload = () => this.addSearchStuff();
+    document.body.appendChild(addsearch_search_ui)
+
+
+    const addsearch_styles = document.createElement("script") // Loads the Addsearch JS blob from them
+    addsearch_styles.setAttribute(
+      "href",
+      `https://cdn.jsdelivr.net/npm/addsearch-search-ui@0.7/dist/addsearch-search-ui.min.css`
+    )
+    addsearch_styles.setAttribute("defer", true)
+    addsearch_styles.setAttribute("rel", 'stylesheet')
+
+    document.body.appendChild(addsearch_styles)
+
+
+}
+ addSearchStuff() {
+   var client = new AddSearchClient('a7b957b7a8f57f4cc544c54f289611c6');
+
+
+   var searchui_conf = {
+     searchResultsPageUrl: 'search'
+   }
+   // Search UI instance
+   var searchui = new AddSearchUI(client, searchui_conf);
+
+   // Add components
+   searchui.searchField({
+     containerId: 'search',
+     searchAsYouType: true,
+     selectorToBind: '.pds-input-field__input',
+     ignoreSearchResultsPageUrl: true,
+   });
+   searchui.searchResults({
+     containerId: 'results'
+   });
+   searchui.pagination({
+     containerId: 'pagination'
+   });
+
+   // All components added. Start
+   searchui.start();
+
+ }
 
   render() {
     return (
@@ -43,13 +73,19 @@ class Search extends React.Component {
         <Container width="wide">
           <main className=" doc-content-well" id="docs-main">
             <div className="">
-              <h1 className="title">Search Results</h1>
+              <h1 className="title">Search Results:</h1>
             </div>
             <div className="" style={{ marginBottom: "15px" }}></div>
             <div className=" mb-70">
               <div className="">
                 <div className="container">
-                  <div className="row"></div>
+                  <div className="row" id="search-field">
+                  </div>
+                  <div className="row addsearch" id="results">
+
+                  </div>
+                  <div className="row" id="pagination">
+                  </div>
                 </div>
               </div>
             </div>
@@ -59,5 +95,6 @@ class Search extends React.Component {
     )
   }
 }
+
 
 export default Search
