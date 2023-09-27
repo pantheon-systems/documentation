7754b3028e55f6b671fef07c6fb1d48c23c94275
M Miller <melissa.miller@pantheon.io>
2023-09-27 15:26:12 -0700

    Update 404.js
    Remove double search from page -- it's already being applied via Layout.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,61 +1,40 @@
 import React from "react"
-import Layout from "../layout/layout"
+import Layout from "../layout/Layout"
 import SEO from "../layout/seo"
 import SVG404 from "../../source/images/404_dark.svg"
 
+import { Container, FlexContainer } from "@pantheon-systems/pds-toolkit-react"
+
 class NotFoundPage extends React.Component {
-  componentDidMount() {
-    //On page load...
-
-    window.addsearch_settings = {
-      search_widget: {
-        placeholder: "Search Pantheon Docs",
-        show_search_suggestions: true,
-        search_suggestion_position: "left",
-        default_sortby: "relevance",
-        display_date: false,
-        display_meta_description: true,
-        display_result_image: false,
-        link_target: "_blank",
-        hide_logo: false,
-        direction: "ltr",
-        api_throttle_time: 2000,
-        automatic_filter_results_by_site_language: false,
-        analytics_enabled: true,
-      },
-    }
-
-    const script = document.createElement("script") // Loads the Addsearch JS blob from them
-    script.setAttribute(
-      "src",
-      `https://cdn.addsearch.com/v5/addsearch-ui.min.js?key=a7b957b7a8f57f4cc544c54f289611c6&id=search_widget`
-    )
-    script.setAttribute("defer", true)
-
-    document.body.appendChild(script)
-  }
-
   render() {
     const { pathname } = this.props.location
 
     return (
-      <Layout type="404">
+      <Layout type="404" containerWidth="narrow">
         <SEO
           title="404"
           description="Zoinks! You've hit a URL that doesn't exist. Let's try a search:"
         />
-        <div style={{ marginTop: "-20px" }} className="container">
-          <main className=" doc-content-well" id="docs-main">
-            <div className="mb-70">
-              <img className="notfound" src={SVG404} />
-
-              <h2>Sorry, there's no page at that URL.</h2>
-            </div>
+        <Container width="narrow">
+          <main id="docs-main">
+            <FlexContainer
+              alignContent="start"
+              alignItems="center"
+              flexDirection="column"
+              mobileFlex="same"
+              className="pds-spacing-mar-block-end-6xl"
+            >
+              <h1>Sorry, there's no page at that URL.</h1>
+              <img
+                className="notfound"
+                style={{ maxWidth: 400 }}
+                src={SVG404}
+              />
+            </FlexContainer>
           </main>
-        </div>
+        </Container>
       </Layout>
     )
-    return <div className="addsearch-container" />
   }
 }
 
