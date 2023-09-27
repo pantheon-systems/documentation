7988659b57bde63b970f0567bfc2146a641470a5
M Miller <melissa.miller@pantheon.io>
2023-09-27 15:13:15 -0700

    Testing with old 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,6 @@ import Layout from "../layout/layout"
 import SEO from "../layout/seo"
 import SVG404 from "../../source/images/404_dark.svg"
 
-import { Container, FlexContainer } from "@pantheon-systems/pds-toolkit-react"
-
 class NotFoundPage extends React.Component {
   componentDidMount() {
     //On page load...
@@ -41,29 +39,20 @@ class NotFoundPage extends React.Component {
     const { pathname } = this.props.location
 
     return (
-      <Layout type="404" containerWidth="narrow">
+      <Layout type="404">
         <SEO
           title="404"
           description="Zoinks! You've hit a URL that doesn't exist. Let's try a search:"
         />
-        <Container width="narrow">
+        <div style={{ marginTop: "-20px" }} className="container">
           <main className=" doc-content-well" id="docs-main">
-            <FlexContainer
-              alignContent="start"
-              alignItems="center"
-              flexDirection="column"
-              mobileFlex="same"
-              className="pds-spacing-mar-block-end-6xl"
-            >
-              <h1>Sorry, there's no page at that URL.</h1>
-              <img
-                className="notfound"
-                style={{ maxWidth: 400 }}
-                src={SVG404}
-              />
-            </FlexContainer>
+            <div className="mb-70">
+              <img className="notfound" src={SVG404} />
+
+              <h2>Sorry, there's no page at that URL.</h2>
+            </div>
           </main>
-        </Container>
+        </div>
       </Layout>
     )
     return <div className="addsearch-container" />
