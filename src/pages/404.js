4d5ecc9278fdf5fd25ae06f7c33afcb795838c9a
M Miller <melissa.miller@pantheon.io>
2023-09-27 17:05:57 -0700

    Update 404.js
    Testing: remove PDS components from page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,6 @@ import Layout from "../layout/layout"
 import SEO from "../layout/seo"
 import SVG404 from "../../source/images/404_dark.svg"
 
-import { Container, FlexContainer } from "@pantheon-systems/pds-toolkit-react"
-
 class NotFoundPage extends React.Component {
   render() {
     const { pathname } = this.props.location
@@ -15,24 +13,18 @@ class NotFoundPage extends React.Component {
           title="404"
           description="Zoinks! You've hit a URL that doesn't exist. Let's try a search:"
         />
-        <Container width="narrow">
+        <div>
           <main id="docs-main">
-            <FlexContainer
-              alignContent="start"
-              alignItems="center"
-              flexDirection="column"
-              mobileFlex="same"
-              className="pds-spacing-mar-block-end-6xl"
-            >
+            <div className="pds-spacing-mar-block-end-6xl">
               <h1>Sorry, there's no page at that URL.</h1>
               <img
                 className="notfound"
                 style={{ maxWidth: 400 }}
                 src={SVG404}
               />
-            </FlexContainer>
+            </div>
           </main>
-        </Container>
+        </div>
       </Layout>
     )
   }
