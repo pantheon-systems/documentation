d37ca861ac00c3fd5969fb77cdb95a50f110fe0a
M Miller <melissa.miller@pantheon.io>
2023-10-10 16:17:54 -0700

    Rework navButtons component to use flex instead; fix markup

--- src/components/navButtons/index.js
+++ src/components/navButtons/index.js
@@ -16,26 +16,24 @@ const NavButtons = ({
       justifyContent="between"
       className="pds-spacing-mar-block-start-m pds-spacing-mar-block-end-xl"
     >
-      {prev && (
-        <ul className="pagination pager-guides">
+      <ul className="pagination pager-guides">
+        {prev && (
           <li className="pagination__prev">
             <Link to={prev} rel="prev" className="pds-button">
               <Icon iconName="angleLeft"></Icon>
               {prevTitle}
             </Link>
           </li>
-        </ul>
-      )}
-      {next && (
-        <ul className="pagination pager-guides">
+        )}
+        {next && (
           <li className="pagination__next">
             <Link to={next} rel="next" className="pds-button">
               {nextTitle}
               <Icon iconName="angleRight"></Icon>
             </Link>
           </li>
-        </ul>
-      )}
+        )}
+      </ul>
     </FlexContainer>
   )
 }
