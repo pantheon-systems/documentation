0a2f793aba92d9f70e5ed6d4f34728af2e33dba3
M Miller <melissa.miller@pantheon.io>
2023-10-27 11:16:42 -0700

    Wistia component - check for window before adding scripts

--- src/components/wistia/index.js
+++ src/components/wistia/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./style.css"
+import { is } from "date-fns/locale"
 /*
 This component creates an embedded Wistia videos.
 
@@ -7,37 +8,49 @@ PARAMETERS:
 src="wistiaHashID"
 */
 
-const Wistia = (props) => {
-  //Construct our component as a function
+const Wistia = ({ src }) => {
+  // Check if window is defined before adding scripts.
+  const isBrowser = typeof window !== "undefined"
 
-  const src = props.src
+  if (isBrowser) {
+    const script2 = document.createElement("script")
 
-  const script2 = document.createElement("script")
-  script2.setAttribute(
-    "src",
-    "https://fast.wistia.com/embed/medias/" + `${src}` + ".jsonp"
-  )
-  script2.setAttribute("async", true)
+    script2.setAttribute(
+      "src",
+      "https://fast.wistia.com/embed/medias/" + `${src}` + ".jsonp"
+    )
+    script2.setAttribute("async", true)
 
-  document.body.appendChild(script2)
+    document.body.appendChild(script2)
 
+    const script = document.createElement("script")
+    script.setAttribute(
+      "src",
+      `https://fast.wistia.com/assets/external/E-v1.js`
+    )
+    script.setAttribute("async", true)
 
-  const script = document.createElement("script")
-  script.setAttribute(
-    "src",
-    `https://fast.wistia.com/assets/external/E-v1.js`
-  )
-  script.setAttribute("async", true)
-
-  document.body.appendChild(script)
+    document.body.appendChild(script)
+  }
 
   return (
     // Return the iframe, wrapped in a div.
     <div class="wistia_responsive_padding">
       <div class="wistia_responsive_wrapper">
-        <div class={'wistia_embed wistia_async_' + `${src}` + " seo=true videoFoam=true"}>
+        <div
+          class={
+            "wistia_embed wistia_async_" + `${src}` + " seo=true videoFoam=true"
+          }
+        >
           <div class="wistia_swatch">
-            <img src={"https://fast.wistia.com/embed/medias/" + `${src}` + "/swatch"} alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" />
+            <img
+              src={
+                "https://fast.wistia.com/embed/medias/" + `${src}` + "/swatch"
+              }
+              alt=""
+              aria-hidden="true"
+              onload="this.parentNode.style.opacity=1;"
+            />
           </div>
         </div>
       </div>
