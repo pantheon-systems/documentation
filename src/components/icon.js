b17620f58606d9e18df3a12b679be7a72ec5b5d7
M Miller <melissa.miller@pantheon.io>
2023-11-27 18:42:56 -0800

    Update icon.js
    Add inline styles, update icon size

--- src/components/icon.js
+++ src/components/icon.js
@@ -3,9 +3,21 @@ import { Icon as PDSIcon } from "@pantheon-systems/pds-toolkit-react"
 
 const Icon = ({ icon, text }) => {
   return (
-    <strong>
-      <PDSIcon iconName={icon} /> {text}
-    </strong>
+    <span
+      style={{
+        display: "inline-flex",
+        alignItems: "center",
+      }}
+    >
+      <strong>
+        <PDSIcon
+          iconName={icon}
+          iconSize="sm"
+          style={{ paddingInline: ".125rem" }}
+        />{" "}
+        {text}
+      </strong>
+    </span>
   )
 }
 export default Icon
