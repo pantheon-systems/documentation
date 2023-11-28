c9f9440ba3f2ff1fe855df57385e0ca88be08258
M Miller <melissa.miller@pantheon.io>
2023-11-28 13:29:26 -0800

    Rework Icon component to allow some legacy icons to use glyphicons
    Also fixes glyphicons and adds size prop

--- src/components/icon.js
+++ src/components/icon.js
@@ -1,8 +1,40 @@
 import React from "react"
 import { Icon as PDSIcon } from "@pantheon-systems/pds-toolkit-react"
 
-const Icon = ({ icon, text }) => {
-  return (
+// List of legacy icons from Hermes that will be not available in the PDS icon set.
+// These icons are rendered using the glyphicons font.
+const legacyIcons = [
+  "charts",
+  "cleaning",
+  "cloud-upload",
+  "equalizer",
+  "server",
+]
+
+// The size prop only affects the PDS icons, not the legacy icons.
+// The legacy icons adapt to the size of the parent container.
+
+const Icon = ({ icon, text, size }) => {
+  return legacyIcons.includes(icon) ? (
+    <span
+      style={{
+        display: "inline-flex",
+        alignItems: "center",
+        height: "0.8rem",
+      }}
+    >
+      <strong>
+        <span
+          className={`glyphicons glyphicons-${icon}`}
+          style={{
+            display: "inline-block",
+            height: "0.8rem",
+          }}
+        />{" "}
+        {text}
+      </strong>
+    </span>
+  ) : (
     <span
       style={{
         display: "inline-flex",
@@ -12,7 +44,7 @@ const Icon = ({ icon, text }) => {
       <strong>
         <PDSIcon
           iconName={icon}
-          iconSize="sm"
+          iconSize={size ? size : "sm"}
           style={{ paddingInline: ".125rem" }}
         />{" "}
         {text}
