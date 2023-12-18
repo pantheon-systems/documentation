09862cbb73a4f1318f0cab026a386ff9289c6347
Steve Persch <steve.persch@pantheon.io>
2023-12-18 13:34:36 -0600

    need to handle building on main

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
+    // TODO, update this url to handle building on the main branch.
     baseURL: process.env.GITHUB_PR_NUMBER ? 'https://pr-' + process.env.GITHUB_PR_NUMBER + '-documentation.appa.pantheon.site' : 'http://localhost:8000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
