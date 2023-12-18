073638dd773bb5f2d27ec0244b10ce76a2f3b1b7
Steve Persch <steve.persch@pantheon.io>
2023-12-18 13:33:39 -0600

    setting base url as variable

--- tests/tests/example.spec.ts
+++ tests/tests/example.spec.ts
@@ -1,19 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://pr-8804-documentation.appa.pantheon.site/release-notes');
+  await page.goto('/release-notes');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Pantheon Release Notes/);
 });
-/*
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-*/
