08168fe3b286cab21773ad3d7179425b35e3744b
Steve Persch <steve.persch@pantheon.io>
2023-12-19 10:21:07 -0600

    updating comments

--- tests/playwright-tests/example.spec.ts
+++ tests/playwright-tests/example.spec.ts
@@ -4,4 +4,6 @@ test('has title', async ({ page }) => {
   await page.goto('/certification');
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Certification Program/);
+
+  /* @todo, add a test of Searching */
 });
