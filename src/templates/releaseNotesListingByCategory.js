c65455e6b30d0463eb8d67228a6d97ce819b6054
Steve Persch <steve.persch@pantheon.io>
2024-01-04 20:23:12 -0600

    loader

--- src/templates/releaseNotesListingByCategory.js
+++ src/templates/releaseNotesListingByCategory.js
@@ -3,9 +3,12 @@ import { graphql } from "gatsby"
 import Layout from "../layout/layout"
 import SEO from "../layout/seo"
 import ReleaseNoteTeaser from "../components/releaseNoteTeaser.js"
-import { releaseNoteCategories } from "../data/releaseNoteCategories.js"
+import { releaseNoteCategoryLoader } from "../data/releaseNoteCategories.js"
 import ReleaseNoteCategorySelector from "../components/releaseNoteCategorySelector.js"
 import { Container } from "@pantheon-systems/pds-toolkit-react"
+
+
+
 
 // Set container width for search and main content.
 const containerWidth = "standard"
@@ -15,7 +18,8 @@ class ReleaseNotesListingByCategoryTemplate extends React.Component {
   render() {
     const releasenotes = this.props.data.allMdx.edges
     const categorySlug = this.props.pageContext.category;
-    const categoryDisplayName = releaseNoteCategories[categorySlug]["displayName"];
+    const categoryData = releaseNoteCategoryLoader(categorySlug);
+    const categoryDisplayName = categoryData["displayName"];
 
     return (
       <Layout containerWidth={containerWidth} footerBorder>
