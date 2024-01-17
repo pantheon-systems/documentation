326ed679322eeb8f291a57ecd314800632a22559
M Miller <melissa.miller@pantheon.io>
2024-01-17 14:40:02 -0800

    Update and style PublishedDate component

--- src/components/releaseNoteTeaser.js
+++ src/components/releaseNoteTeaser.js
@@ -2,29 +2,32 @@ import React from "react"
 import { Link } from "gatsby"
 import ReleaseNoteCategories from "../components/releaseNoteCategories"
 import MdxWrapper from "../components/mdxWrapper.js"
-import FormattedPublishedDate from "./formattedPublishedDate.js"
+import PublishedDate from "./PublishedDate"
 
 const ReleaseNoteTeaser = ({ ReleaseNoteData }) => {
-
   if (!ReleaseNoteData) {
     return null
   }
 
   return (
     <React.Fragment key={ReleaseNoteData.id}>
-    <div>
-      <Link
-        to={`/${ReleaseNoteData.fields.slug}`}
-        className="individual-changelog-link"
-      >
-        <h2 id={ReleaseNoteData.fields.slug}>
-          {ReleaseNoteData.frontmatter.title}
-        </h2>
-      </Link>
-        <ReleaseNoteCategories categories={ReleaseNoteData.frontmatter.categories} />
-      <FormattedPublishedDate dateString={ReleaseNoteData.frontmatter.published_date} />
-      <MdxWrapper mdx={ReleaseNoteData.body} />
-    </div>
+      <div>
+        <Link
+          to={`/${ReleaseNoteData.fields.slug}`}
+          className="individual-changelog-link"
+        >
+          <h2 id={ReleaseNoteData.fields.slug}>
+            {ReleaseNoteData.frontmatter.title}
+          </h2>
+        </Link>
+        <ReleaseNoteCategories
+          categories={ReleaseNoteData.frontmatter.categories}
+        />
+        <PublishedDate
+          dateString={ReleaseNoteData.frontmatter.published_date}
+        />
+        <MdxWrapper mdx={ReleaseNoteData.body} />
+      </div>
     </React.Fragment>
   )
 }
