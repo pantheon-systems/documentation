e3b409c81545e42fb062609b144d92f9c6b20098
M Miller <melissa.miller@pantheon.io>
2024-01-17 15:35:26 -0800

    Update releaseNotePage.js
    clean up and style

--- src/templates/releaseNotePage.js
+++ src/templates/releaseNotePage.js
@@ -2,21 +2,17 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../layout/layout"
 import SEO from "../layout/seo"
-import ReleaseNoteCategories from "../components/releaseNoteCategories"
+import ReleaseNoteCategories from "../components/ReleaseNoteCategories"
 import MdxWrapper from "../components/mdxWrapper.js"
-import FormattedPublishedDate from "../components/formattedPublishedDate.js"
+import PublishedDate from "../components/PublishedDate"
 // This is used to get the fields from the MDX file.
 import { releaseNoteFragment } from "../fragments/releaseNote.js"
-import {
-  Container,
-  SidebarLayout,
-} from "@pantheon-systems/pds-toolkit-react"
+import { Container, SidebarLayout } from "@pantheon-systems/pds-toolkit-react"
 
 // Set container width for search and main content.
-const containerWidth = "standard"
+const containerWidth = "narrow"
 
 class ReleaseNoteTemplate extends React.Component {
-
   render() {
     const node = this.props.data.mdx
 
@@ -28,27 +24,30 @@ class ReleaseNoteTemplate extends React.Component {
           authors={node.frontmatter.contributors}
           image={"/images/assets/default-thumb-doc.png"}
         />
-        <main id="docs-main" tabindex="-1">
+        <main id="docs-main" tabIndex="-1">
           <Container width={containerWidth}>
             <div className="pds-overline-text pds-spacing-mar-block-end-xs">
               Pantheon Release Notes
             </div>
-            <h1>{node.frontmatter.title}</h1>
-            <ReleaseNoteCategories categories={node.frontmatter.categories} />
-            <hr />
-            <SidebarLayout sidebarMobileLocation="before">
-              <article
-                slot="content"
-                className="changelog changelog--individual"
-              >
-                <div id="doc" className="doc changelog__content">
-                    <div className="pds-spacing-mar-block-start-s pds-spacing-mar-block-end-2xl">
-                      <FormattedPublishedDate dateString={node.frontmatter.published_date} />
-                      <MdxWrapper mdx={node.body} />
-                    </div>
+            <h1 className="pds-spacing-mar-block-end-l">
+              {node.frontmatter.title}
+            </h1>
+            <ReleaseNoteCategories
+              categories={node.frontmatter.categories}
+              className="pds-spacing-mar-block-end-xl"
+            />
+
+            <article>
+              <div id="doc" className="doc changelog__content">
+                <div className="pds-spacing-mar-block-start-s pds-spacing-mar-block-end-2xl">
+                  <PublishedDate
+                    dateString={node.frontmatter.published_date}
+                    className="pds-spacing-mar-block-end-m"
+                  />
+                  <MdxWrapper mdx={node.body} />
                 </div>
-              </article>
-            </SidebarLayout>
+              </div>
+            </article>
           </Container>
         </main>
       </Layout>
@@ -59,8 +58,9 @@ class ReleaseNoteTemplate extends React.Component {
 export default ReleaseNoteTemplate
 
 export const pageQuery = graphql`
-query ReleaseNoteBySlug($slug: String!) {
+  query ReleaseNoteBySlug($slug: String!) {
     mdx(fields: { slug: { eq: $slug } }) {
       ...theReleaseNoteFields
     }
-  }`
+  }
+`
