54465c67323420b1fbe1c0a044c8a5cf2c061310
M Miller <melissa.miller@pantheon.io>
2024-01-19 18:54:32 -0800

    Update releaseNoteCategorySelector.js
    Configure to use only active categories

--- src/components/releaseNoteCategorySelector.js
+++ src/components/releaseNoteCategorySelector.js
@@ -1,37 +1,48 @@
 import React from "react"
-import releaseNoteCategoriesObject from "../../source/releasenotescategories/releaseNoteCategories.json"
+import { activeReleaseNoteCategories } from "../data/releaseNoteCategories"
 import { MenuButton } from "@pantheon-systems/pds-toolkit-react"
 import { Link } from "gatsby"
 
-// Initialize the links array with the All Release Notes link and a separator.
-const categoryMenuItems = [
-  {
-    isLink: true,
-    linkContent: <Link to={`/release-notes`}>All Release Notes</Link>,
-  },
-  {
-    isSeparator: true,
-  },
-]
+const ReleaseNoteCategorySelector = () => {
+  // Initialize the links array with the All Release Notes link and a separator.
+  const categoryMenuItems = [
+    {
+      isLink: true,
+      linkContent: <Link to={`/release-notes`}>All Release Notes</Link>,
+    },
+    {
+      isSeparator: true,
+    },
+  ]
 
-// Map over the categories and add each to the links array.
-releaseNoteCategoriesObject.categories.map((category) => {
-  categoryMenuItems.push({
-    isLink: true,
-    linkContent: (
-      <Link to={`/release-notes/${category.slug}`}>{category.displayName}</Link>
-    ),
-  })
-})
+  // Function to create menu items for each valid category in use.
+  const createCategoryMenuItems = (categoryObjects) => {
+    categoryObjects.map((category) => {
+      categoryMenuItems.push({
+        isLink: true,
+        linkContent: (
+          <Link to={`/release-notes/${category.slug}`}>
+            {category.displayName}
+          </Link>
+        ),
+      })
+    })
+  }
 
-// Create the menu button with the links array.
-const ReleaseNoteCategorySelector = (currentCategorySlug) => {
+  // Get the active categories data.
+  const activeCategories = JSON.parse(activeReleaseNoteCategories())
+
+  // Create the menu items.
+  createCategoryMenuItems(activeCategories)
+
   return (
-    <MenuButton
-      label="Categories"
-      variant="secondary"
-      menuItems={categoryMenuItems}
-    />
+    <>
+      <MenuButton
+        label="Categories"
+        variant="secondary"
+        menuItems={categoryMenuItems}
+      />
+    </>
   )
 }
 
