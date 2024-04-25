175ed74bdeb9d9223fa29a9c3624e529a99be4e0
Steve Persch <steve.persch@pantheon.io>
2024-04-24 21:08:57 -0500

    stripping down iframeembed

--- src/templates/iframeembed.js
+++ src/templates/iframeembed.js
@@ -9,29 +9,25 @@ const containerWidth = "standard"
 
 class iFrameEmbedTemplate extends React.Component {
   render() {
-    console.log(this.props);
     const data = this.props.pageContext;
 
     return (
-      <Layout containerWidth={containerWidth} excludeSearch footerBorder>
-        <SEO  />
+
         <main id="docs-main" tabIndex="-1">
           <Container
             width={containerWidth}
             className="pds-spacing-mar-block-start-3xl"
           >
-
             <article className="pds-spacing-pad-block-end-xl">
               <div id="doc" className="doc changelog__content">
                 <div className="pds-spacing-mar-block-start-s pds-spacing-mar-block-end-2xl">
-
                   <MdxWrapper mdx={data.body} />
                 </div>
               </div>
             </article>
           </Container>
         </main>
-      </Layout>
+
     )
   }
 }
