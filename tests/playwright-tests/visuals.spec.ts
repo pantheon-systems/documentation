c113ea13cdff8acda52125fdeb88eab07fecabcc
Steve Persch <steve.persch@pantheon.io>
2024-05-20 15:50:07 -0500

    screenshotting more URLs

--- tests/playwright-tests/visuals.spec.ts
+++ tests/playwright-tests/visuals.spec.ts
@@ -6,12 +6,50 @@ test('home page', async ({ page }) => {
   await expect(page).toHaveScreenshot('homepage.png', { fullPage: true });
 });
 
-test('kill-mysql-queries', async ({ page }) => {
-  await page.goto('/guides/mariadb-mysql/kill-mysql-queries');
+
+test('Get Started - Landing Page', async ({ page }) => {
+  await page.goto('/get-started/');
   await page.getByText('Accept Cookies').click();
-  await expect(page.locator('#toc')).toBeVisible();
-  await expect(page).toHaveScreenshot('guides---mariadb-mysql---kill-mysql-queries.png', { fullPage: true });
+  await expect(page).toHaveScreenshot('get-started.png', { fullPage: true });
 });
+
+
+test('Release Notes - Category Listing', async ({ page }) => {
+  await page.goto('/release-notes/policy');
+  await page.getByText('Accept Cookies').click();
+  await expect(page).toHaveScreenshot('release-notes--category.png', { fullPage: true });
+});
+
+test('Release Notes - Single Entry', async ({ page }) => {
+  await page.goto('/release-notes/2024/05/new-relic-agent-update-released');
+  await page.getByText('Accept Cookies').click();
+  await expect(page).toHaveScreenshot('release-notes--single-entry.png', { fullPage: true });
+});
+
+test('Terminus Manual - Single Page', async ({ page }) => {
+  await page.goto('/terminus/install');
+  await page.getByText('Accept Cookies').click();
+  await expect(page).toHaveScreenshot('terminus-install.png', { fullPage: true });
+});
+
+test('Terminus Manual - Single Command', async ({ page }) => {
+  await page.goto('/terminus/commands/completion');
+  await page.getByText('Accept Cookies').click();
+  await expect(page).toHaveScreenshot('terminus-single-command.png', { fullPage: true });
+});
+
+test('Certification Landing Page', async ({ page }) => {
+  await page.goto('/certification');
+  await page.getByText('Accept Cookies').click();
+  await expect(page).toHaveScreenshot('certification-landing-page.png', { fullPage: true });
+});
+
+test('Glossary', async ({ page }) => {
+  await page.goto('/glossary');
+  await page.getByText('Accept Cookies').click();
+  await expect(page).toHaveScreenshot('glossary.png', { fullPage: true });
+});
+
 
 test('certification---study-guide', async ({ page }) => {
   await page.goto('/certification/study-guide');
--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -39,12 +39,10 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
