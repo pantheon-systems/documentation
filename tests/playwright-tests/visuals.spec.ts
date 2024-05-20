94739d5860fef7ba9de03f18d5bc99c25046d8c4
Steve Persch <steve.persch@pantheon.io>
2024-05-20 15:38:12 -0500

    full page screenshots. TOC handling

--- tests/playwright-tests/visuals.spec.ts
+++ tests/playwright-tests/visuals.spec.ts
@@ -8,26 +8,21 @@ test('home page', async ({ page }) => {
 
 test('kill-mysql-queries', async ({ page }) => {
   await page.goto('/guides/mariadb-mysql/kill-mysql-queries');
-
   await page.getByText('Accept Cookies').click();
-
- // await expect(page.getByLabel('toc-nav')).toBeVisible();
-
-  await expect(page).toHaveScreenshot('guides---mariadb-mysql---kill-mysql-queries.png');
+  await expect(page.locator('#toc')).toBeVisible();
+  await expect(page).toHaveScreenshot('guides---mariadb-mysql---kill-mysql-queries.png', { fullPage: true });
 });
 
 test('certification---study-guide', async ({ page }) => {
   await page.goto('/certification/study-guide');
-//  await expect(page.getByLabel('toc-nav')).toBeVisible();
   await page.getByText('Accept Cookies').click();
-
-  await expect(page).toHaveScreenshot('certification---study-guide.png');
+  await expect(page.locator('#toc')).toBeVisible();
+  await expect(page).toHaveScreenshot('certification---study-guide.png', { fullPage: true } );
 });
 
 test('drupal-cron', async ({ page }) => {
   await page.goto('/drupal-cron');
-//  await expect(page.getByLabel('toc-nav')).toBeVisible();
   await page.getByText('Accept Cookies').click();
-
-await expect(page).toHaveScreenshot('drupal-cron.png');
+  await expect(page.locator('#toc')).toBeVisible();
+  await expect(page).toHaveScreenshot('drupal-cron.png', { fullPage: true });
 });
