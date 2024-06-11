9fe9b7e694b8f3914c36001ce229555387f3edca
Rachel Whitton <rachel@pantheon.io>
2024-06-11 09:42:45 -0500

    Fix 404 for release notes category listing

--- tests/playwright-tests/visuals.spec.ts
+++ tests/playwright-tests/visuals.spec.ts
@@ -15,7 +15,7 @@ test('Get Started - Landing Page', async ({ page }) => {
 
 
 test('Release Notes - Category Listing', async ({ page }) => {
-  await page.goto('/release-notes/policy');
+  await page.goto('/release-notes/?category=policy');
   await page.getByText('Accept Cookies').click();
   await expect(page).toHaveScreenshot('release-notes--category.png', { fullPage: true });
 });
