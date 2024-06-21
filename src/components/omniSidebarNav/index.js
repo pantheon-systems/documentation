3c2581ccddfad28ff84302959f39b5dd218b53c7
Steve Persch <steve.persch@pantheon.io>
2024-06-21 10:44:13 -0500

    submenus

--- src/components/omniSidebarNav/index.js
+++ src/components/omniSidebarNav/index.js
@@ -1,78 +1,12 @@
 import React from "react"
 
 import { SidebarNav, turnItemsIntoLinks } from "../sidebarNav"
+import CertificationItems from "./submenus/certification";
 
 
 const OmniSidebarNav = ({activePage, maximumParent}) => {
 
-
-  const CertificationItems = [
-    {
-      link: "/certification/about",
-      title: "About the Certification Program",
-    },
-    {
-      link: "/certification/exam",
-      title: "Taking the Exam",
-    },
-    {
-      link: "/certification/study-guide",
-      title: "Study Guide",
-      children: [
-        {
-          link: "/certification/study-guide",
-          title: "Introduction",
-        },
-        {
-          link: "/certification/study-guide/webops",
-          title: "Chapter 1: WebOps",
-        },
-        {
-          link: "/certification/study-guide/platform",
-          title: "Chapter 2: Pantheon Platform",
-        },
-        {
-          link: "/certification/study-guide/create",
-          title: "Chapter 3: Site Creation",
-        },
-        {
-          link: "/certification/study-guide/cdn",
-          title: "Chapter 4: Content Delivery Network",
-        },
-        {
-          link: "/certification/study-guide/cms",
-          title: "Chapter 5: CMS Infrastructure",
-        },
-        {
-          link: "/certification/study-guide/deploy",
-          title: "Chapter 6: The Deployment Pipeline",
-        },
-        {
-          link: "/certification/study-guide/people",
-          title: "Chapter 7: Connecting People",
-        },
-        {
-          link: "/certification/study-guide/extend",
-          title: "Chapter 8: Extend with CLI and Hooks",
-        },
-        {
-          link: "/certification/study-guide/automate",
-          title: "Chapter 9: Additional Automation",
-        },
-        {
-          link: "/certification/study-guide/custom-upstreams",
-          title: "Chapter 10: Custom Upstreams",
-        },
-      ]
-    },
-    {
-      link: "https://certification.pantheon.io/",
-      title: "↗ Certification Directory",
-    },
-  ]
-
   const OmniItems = [
-
     {
       link: "/get-started",
       title: "Get Started",
@@ -83,8 +17,6 @@ const OmniSidebarNav = ({activePage, maximumParent}) => {
       children: CertificationItems
     }
   ];
-
-
 
   function findSubMenuItemsToUse(maximumParent, NestedItems) {
     for (let item of NestedItems) {
@@ -100,17 +32,13 @@ const OmniSidebarNav = ({activePage, maximumParent}) => {
 
   const activeParent = [findSubMenuItemsToUse(maximumParent, OmniItems)];
 
-
-  console.log(activeParent);
-
   const OmniLinks = turnItemsIntoLinks(activeParent, activePage);
-
 
   return (
     <SidebarNav
       slot="guide-menu"
-      title="WebOps Certification"
-      links={OmniLinks} />
+      title={OmniLinks[0].linkContent}
+      links={OmniLinks[0].links} />
   )
 }
 
