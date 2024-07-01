3f1cc9cab1a29fa65f863e21f299191e05eb4049
Steve Persch <steve.persch@pantheon.io>
2024-07-01 18:14:22 -0500

    manual formatting

--- src/components/omniSidebarNav/getGuideDirectory.js
+++ src/components/omniSidebarNav/getGuideDirectory.js
@@ -7,21 +7,11 @@ const getAllGuidePages = () => {
       {
         allGuides: allMdx(
           filter: {
-            fileAbsolutePath: {
-              ne: null
-            }
-            fields: {
-              guide_directory: {
-                regex: "/^(?=guides\/).*$/i"
-              }
-            },
-          frontmatter: {
-            draft: {
-              ne: true
-            }
+            fileAbsolutePath: { ne: null }
+            fields: { guide_directory: { regex: "/^(?=guides\/).*$/i" } },
+            frontmatter: { draft: { ne: true } }
           }
-        }
-      sort: { fields: [fileAbsolutePath], order: ASC }
+          sort: { fields: [fileAbsolutePath], order: ASC }
     ) {
       edges {
         node {
