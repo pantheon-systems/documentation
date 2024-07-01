708a88b6a988423b8df7113f632c210e49d1476a
Steve Persch <steve.persch@pantheon.io>
2024-07-01 18:16:53 -0500

    prettier with manual fix

--- src/components/omniSidebarNav/getGuideDirectory.js
+++ src/components/omniSidebarNav/getGuideDirectory.js
@@ -1,38 +1,37 @@
 import { useStaticQuery, graphql } from 'gatsby';
 
 const getAllGuidePages = () => {
-
   const AllTheGuides = useStaticQuery(
     graphql`
       {
         allGuides: allMdx(
           filter: {
             fileAbsolutePath: { ne: null }
-            fields: { guide_directory: { regex: "/^(?=guides\/).*$/i" } },
+            fields: { guide_directory: { regex: "/^(?=guides\/).*$/i" } }
             frontmatter: { draft: { ne: true } }
           }
           sort: { fields: [fileAbsolutePath], order: ASC }
-    ) {
-      edges {
-        node {
-          id
-          fields {
-            slug
-            guide_directory
-          }
-          frontmatter {
-            title
-            subtitle
+        ) {
+          edges {
+            node {
+              id
+              fields {
+                slug
+                guide_directory
+              }
+              frontmatter {
+                title
+                subtitle
+              }
+            }
           }
         }
       }
-    }
-      }
-    `
+    `,
   );
 
   return AllTheGuides;
-}
+};
 
 function getChildrenForGuideDirectory(AllTheGuides, guideDirectory) {
   const ChildItems = [];
