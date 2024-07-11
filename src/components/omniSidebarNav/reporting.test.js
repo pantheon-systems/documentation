c8dcac550fc5fa5eda39e81584ab311f6727b6f9
Steve Persch <steve.persch@pantheon.io>
2024-07-10 20:56:34 -0500

    more reporting tools

--- src/components/omniSidebarNav/reporting.test.js
+++ src/components/omniSidebarNav/reporting.test.js
@@ -7,7 +7,11 @@ import { expect, test } from 'vitest';
 // import AllGuides from './testfixtures/allGuides.testfixture';
 // import guideDirectoryItems from './testfixtures/guideDirectoryItems.textfixture';
 import omniItems from './testfixtures/omniItems.textfixture';
+import allPaths from './testfixtures/allPaths.testfixture';
 // import activeSection from './testfixtures/activeSection.testfixture';
+
+const exceptions = ['https://certification.pantheon.io/'];
+
 
 function flattenOmniItems(menuItems) {
   // loop over the omniItems and make a flattened array of all the links
@@ -23,8 +27,6 @@ function flattenOmniItems(menuItems) {
 
   // return flattened with duplicates removed
   return [...new Set(flattened)];
-
-//  return flattened;
 }
 
 
@@ -34,3 +36,26 @@ test('Check that the deep array/object of onmiItems can be flattened and contain
   // It just so happens that the testfixtures/omniItems.textfixture.js has 31 links.
   expect(flattened.length).toEqual(28);
 });
+
+
+test('Check that all items in the flattened menu list are present in the list of all written paths', () => {
+  const flattened = flattenOmniItems(omniItems);
+
+  // merge allWrittenPaths and exceptions
+  const allWrittenPathsAndExceptions = allPaths.concat(exceptions);
+  // Loop over flattened and check that each item is in allWrittenPaths
+  for (let item of flattened) {
+    expect(allWrittenPathsAndExceptions).toContain(item);
+  }
+});
+
+test('Calculate the percentage of written paths that are not in the menu or exceptions', () => {
+  const flattened = flattenOmniItems(omniItems);
+  // merge allWrittenPaths and exceptions
+  const allWrittenPathsAndExceptions = allPaths.concat(exceptions);
+  // Loop over flattened and check that each item is in allWrittenPaths
+  for (let item of flattened) {
+    expect(allWrittenPathsAndExceptions).toContain(item);
+  }
+});
+
