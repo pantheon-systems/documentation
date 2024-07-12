c6063daf9a7d3e1623e3d21626b667af6dbb1d6c
gfbollingerHakuna <88794506+gfbollingerHakuna@users.noreply.github.com>
2024-07-12 14:07:40 -0300

    New Site Feature: RSS Feed for Release Notes (#9034)
    * WECS-164: Add `gatsby-plugin-feed` config to create release notes rss feed
    
    * WECS-164: Fix description and id
    
    * WECS-164: Add rss link in release-notes page template
    
    * WECS-164: Adjust RSS feed options
    
    * WECS-164: Change rss feed title
    
    * WECS-164: Update pds-toolkit-react version
    
    * WECS-164: Add rss link to release-note page template and modify Popover z-index
    
    * update toolkit to dev.130
    
    This fixes an error on local development with the InputText component.
    
    * Update pds-input classes on release notes filter
    
    ---------
    
    Co-authored-by: M Miller <melissa.miller@pantheon.io>

--- src/templates/releaseNotesListing/index.js
+++ src/templates/releaseNotesListing/index.js
@@ -206,7 +206,7 @@ const ReleaseNotesListingTemplate = ({ data }) => {
     initialLoadRef.current = false
   },[])
 
-  
+
   // Debounce search input.
   const debouncedHandleInputChange = debounce(handleInputChange, 300)
 
@@ -246,6 +246,10 @@ const ReleaseNotesListingTemplate = ({ data }) => {
         >
           <h1>Pantheon release notes</h1>
           <div className="pds-lead-text pds-lead-text--sm">{introText}</div>
+          <a href="/release-notes/rss.xml" target="_blank" className="rss-feed-link">
+            <Icon className="rss-feed-link-icon" iconName='rss' iconSize='lg' />
+            <span>Subscribe to RSS feed</span>
+          </a>
           <div
             style={{
               borderBottom: "1px solid var(--pds-color-border-default)",
@@ -254,7 +258,7 @@ const ReleaseNotesListingTemplate = ({ data }) => {
             }}
           >
             <div
-              className="pds-input-field__input-wrapper pds-spacing-mar-block-end-xl"
+              className="pds-input-field pds-input-field--text pds-spacing-mar-block-end-xl"
               style={{
                 flexGrow: "2",
               }}
