e2d3f130376212d7c0f8412a7f9d2f7884139b2f
Steve Persch <steve.persch@pantheon.io>
2024-10-07 17:35:40 -0500

    addsearch back

--- src/components/addSearch.js
+++ src/components/addSearch.js
@@ -23,7 +23,8 @@ class AddSearch extends React.Component {
     const script = document.createElement("script")
     script.setAttribute(
       "src",
-      `https://cdn.addsearch.com/v5/addsearch-ui.min.js?key=a7b957b7a8f57f4cc544c54f289611c6&id=search_widget${resultPage.includes("search") ? "&type=resultpage" : ""
+      `https://cdn.addsearch.com/v5/addsearch-ui.min.js?key=a7b957b7a8f57f4cc544c54f289611c6&id=search_widget${
+        resultPage.includes("search") ? "&type=resultpage" : ""
       }`
     )
     script.setAttribute("defer", true)
