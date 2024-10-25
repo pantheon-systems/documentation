56488b2ef5884dfe9d6e97221c8e44e37b5456cb
Rachel Whitton <rachel@pantheon.io>
2024-10-25 15:24:03 -0500

    Combine migration and getstarted submenus

--- src/components/omniSidebarNav/submenus/migrateAndUpgrade.js
+++ src/components/omniSidebarNav/submenus/migrateAndUpgrade.js
@@ -2,20 +2,10 @@ import { getGuideDirectory, simpleLink } from './../helpers';
 
 const migrateAndUpgrade = () => {
   return {
-    link: '/guides/guided',
+    link: '/guides/guidasdfasdfed',
     title: 'Migrating Sites to Pantheon',
     children: [
-      getGuideDirectory('guides/migrate/guided', 'Guided Migration'),
-      simpleLink('/migrate-manual', 'Manual Migration'),
-      simpleLink('/migrate-wordpress-multisite', 'WordPress Multisite'),
-      getGuideDirectory('guides/migrate/kinsta', 'Kinsta'),
-      getGuideDirectory('guides/migrate/pagely', 'Pagely'),
-      getGuideDirectory('guides/migrate/wordpressvip', 'WordPress VIP'),
-      getGuideDirectory('guides/migrate/wpengine', 'WP Engine'),
-      /// I would have guessed that the Platform.sh docs would be about Drupal, not WordPress.
-      getGuideDirectory('guides/migrate/platformsh', 'Platform.sh'),
-      getGuideDirectory('guides/migrate/acquia', 'Acquia'),
-      simpleLink('/migrate-cpanel', 'GoDaddy CPanel'),
+      
     ],
   };
 };
