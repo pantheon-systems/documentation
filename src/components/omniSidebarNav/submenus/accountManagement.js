57fbed1697841da704a56ab2bb27db582e62448a
Rachel Whitton <rachel@pantheon.io>
2024-10-29 15:38:05 -0500

    Apply Prettier formatting

--- src/components/omniSidebarNav/submenus/accountManagement.js
+++ src/components/omniSidebarNav/submenus/accountManagement.js
@@ -5,9 +5,8 @@ const accountManagement = () => {
     link: '/guides/account-mgmt/account',
     title: 'Manage Accounts and Workspaces',
     children: [
-
       simpleLink('/guides/account-mgmt/account', 'Billing', [
-        getGuideDirectory('guides/account-mgmt/account'), 
+        getGuideDirectory('guides/account-mgmt/account'),
         getGuideDirectory('guides/account-mgmt/billing'),
         getGuideDirectory('guides/enterprise-billing-center'),
         getGuideDirectory('guides/account-mgmt/plans'),
@@ -17,14 +16,18 @@ const accountManagement = () => {
       simpleLink('/guides/sso', 'Single Sign-On', [
         getGuideDirectory('guides/sso'),
         simpleLink('/ldap-and-ldaps', 'LDAP and LDAPS'),
-        getGuideDirectory('guides/wordpress-google-sso', 'WP SAML Auth'),  
+        getGuideDirectory('guides/wordpress-google-sso', 'WP SAML Auth'),
       ]),
-      simpleLink('/guides/account-mgmt/workspace-sites-teams', 'Portfolio Governance', [
-        getGuideDirectory('guides/account-mgmt/workspace-sites-teams'),
-        getGuideDirectory('guides/custom-upstream'),
-        getGuideDirectory('guides/autopilot', 'Autopilot Guide'),
-        getGuideDirectory('guides/autopilot-custom-upstream'),
-      ]),
+      simpleLink(
+        '/guides/account-mgmt/workspace-sites-teams',
+        'Portfolio Governance',
+        [
+          getGuideDirectory('guides/account-mgmt/workspace-sites-teams'),
+          getGuideDirectory('guides/custom-upstream'),
+          getGuideDirectory('guides/autopilot', 'Autopilot Guide'),
+          getGuideDirectory('guides/autopilot-custom-upstream'),
+        ],
+      ),
     ],
   };
 };
