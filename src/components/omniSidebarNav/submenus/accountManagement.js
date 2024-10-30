fd3e9b85192e4f0e19a00944f36274ddd336a7fc
Rachel Whitton <rachel@pantheon.io>
2024-10-30 14:07:49 -0500

    Apply Prettier formatting

--- src/components/omniSidebarNav/submenus/accountManagement.js
+++ src/components/omniSidebarNav/submenus/accountManagement.js
@@ -11,7 +11,7 @@ const accountManagement = () => {
         getGuideDirectory('guides/enterprise-billing-center'),
         getGuideDirectory('guides/account-mgmt/plans'),
         getGuideDirectory('guides/account-mgmt/traffic'),
-        // Consider relocating this next one: 
+        // Consider relocating this next one:
         simpleLink('/tax-exempt-status', 'Tax Exempt Status'),
       ]),
       simpleLink('/guides/sso', 'Single Sign-On', [
