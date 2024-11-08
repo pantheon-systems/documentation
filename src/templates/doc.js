fe4925e7b9eb44faf25958d3cc54d31acccfc64d
Steve Persch <stevector@users.noreply.github.com>
2024-11-08 16:31:00 -0600

    remove dead code for popovers

--- src/templates/doc.js
+++ src/templates/doc.js
@@ -16,32 +16,6 @@ import MdxWrapper from "../components/mdxWrapper"
 const containerWidth = "standard"
 
 class DocTemplate extends React.Component {
-  componentDidMount() {
-    $("[data-toggle=popover]").popover({
-      trigger: "click",
-      placement: "right",
-    })
-
-    $("body").on("click", function (e) {
-      $('[data-toggle="popover"]').each(function () {
-        if (
-          !$(this).is(e.target) &&
-          $(this).has(e.target).length === 0 &&
-          $(".popover").has(e.target).length === 0
-        ) {
-          $(this).popover("hide")
-        }
-      })
-    })
-
-    $("body").keyup(function (e) {
-      $('[data-toggle="popover"]').each(function () {
-        if (event.which === 27) {
-          $(this).popover("hide")
-        }
-      })
-    })
-  }
 
   render() {
     const node = this.props.data.doc
