fe53735c5ba599584e4e8d7c848afd47f07e42b1
Chris Reynolds <chris@jazzsequence.com>
2024-12-17 17:09:50 -0700

    maybe fix filtering again

--- src/components/releases.js
+++ src/components/releases.js
@@ -15,29 +15,31 @@ const shortcodes = {
 const Releases = ({ data }) => {
   const oneYearAgo = subYears(new Date(), 1);
 
-  // Filter releases published within the last year
+  // Safe Filtering: Ensure `published_at` exists before filtering
   const filteredReleases = data.allTerminusReleasesJson.edges.filter(
     (release) => {
       const publishedDate = release.node.published_at;
       return publishedDate && isAfter(parseISO(publishedDate), oneYearAgo);
-    },
+    }
   );
 
   return (
     <>
-      {filteredReleases.map((release, i) => (
-        <div key={i}>
-          <h3 className="toc-ignore" id={release.node.tag_name}>
-            {release.node.tag_name}
-          </h3>
-          <MDXProvider components={shortcodes}>
-            <MDXRenderer>
-              {release.node.fields.markdownBody.childMdx.body}
-            </MDXRenderer>
-          </MDXProvider>
-          <hr />
-        </div>
-      ))}
+      {filteredReleases.length > 0 ? (
+        filteredReleases.map((release, i) => (
+          <div key={i}>
+            <h3 className="toc-ignore" id={release.node.tag_name}>
+              {release.node.tag_name}
+            </h3>
+            <MDXProvider components={shortcodes}>
+              <MDXRenderer>{release.node.fields.markdownBody.childMdx.body}</MDXRenderer>
+            </MDXProvider>
+            <hr />
+          </div>
+        ))
+      ) : (
+        <p>No recent releases found.</p>
+      )}
     </>
   );
 };
