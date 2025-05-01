fe83f37c78557b8ef36e83bff33f29cbbe1cd281
Rachel Whitton <rachel@pantheon.io>
2025-05-01 16:36:18 -0500

    Remove now deleted doc from submenu

--- src/components/omniSidebarNav/submenus/security.js
+++ src/components/omniSidebarNav/submenus/security.js
@@ -12,10 +12,6 @@ const security = () => {
       simpleLink('/certificate-bundles', 'Certificate Bundles'),
       simpleLink('/clamav', 'ClamAV'),
       simpleLink('/drupal-security-patches', 'Drupal Security Patches'),
-      simpleLink(
-        '/interstitial-pages-on-sandbox-sites',
-        'Interstitial Warnings on Sandbox Sites',
-      ),
     ],
   };
 };
