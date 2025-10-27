31a6764c8235117434001f87307e890658c44f61
Steve Persch <steve.persch@VWK3VMWXVG-steve.persch>
2025-10-27 15:07:25 -0500

    moving links

--- src/components/omniSidebarNav/submenus/webInfrastructure.js
+++ src/components/omniSidebarNav/submenus/webInfrastructure.js
@@ -148,24 +148,6 @@ const webInfrastructure = () => {
           'Optimize wp_options Table and Autoloaded Data',
         ),
       ]),
-      simpleLink('/nextjs', 'Next.js', [
-        simpleLink('/nextjs', 'Next.js Overview'),
-        simpleLink('/nextjs/architecture', 'Architecture'),
-        simpleLink('/nextjs/hello-world-tutorial', 'Tutorial: Hello World'),
-        simpleLink(
-          '/nextjs/content-publisher-tutorial',
-          'Tutorial: Content Publisher',
-        ),
-        simpleLink('/nextjs/environment-variables', 'Environment Variables'),
-        simpleLink('/nextjs/cli-tools', 'CLI Tools'),
-        simpleLink(
-          '/nextjs/migrating-from-front-end-sites',
-          'Migrating from Front-End Sites',
-        ),
-        simpleLink('/nextjs/test-and-live-env', 'Test and Live Environments'),
-        simpleLink('/nextjs/connecting-custom-domain-name', 'Connecting a Custom Domain Name'),
-        simpleLink('/nextjs/request-access', 'Private Alpha Program'),
-      ]),
       {
         link: '/guides/decoupled',
         title: 'Front-End Sites and Starter Kits',
