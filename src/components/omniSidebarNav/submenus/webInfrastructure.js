b0c4afd74cf57f748ca413f48001fdd69ebba10e
Steve Persch <steve.persch@pantheon.io>
2025-10-29 16:06:38 -0500

    moving nextjs back

--- src/components/omniSidebarNav/submenus/webInfrastructure.js
+++ src/components/omniSidebarNav/submenus/webInfrastructure.js
@@ -148,6 +148,38 @@ const webInfrastructure = () => {
           'Optimize wp_options Table and Autoloaded Data',
         ),
       ]),
+
+      simpleLink('/nextjs', 'Next.js', [
+        simpleLink('/nextjs', 'Next.js Overview'),
+        simpleLink('/nextjs/architecture', 'Architecture and Git Workflow', [
+          simpleLink('/nextjs/architecture', 'Architecture'),
+          simpleLink('/nextjs/multidev', 'Multidev environments'),
+          simpleLink('/nextjs/test-and-live-env', 'Test and Live Environments'),
+          simpleLink('/nextjs/cli-tools', 'CLI Tools'),
+        ]),
+
+        simpleLink('/nextjs/hello-world-tutorial', 'Tutorials and How-Tos', [
+          simpleLink('/nextjs/hello-world-tutorial', 'Tutorial: Hello World'),
+          simpleLink(
+            '/nextjs/content-publisher-tutorial',
+            'Tutorial: Content Publisher',
+          ),
+          simpleLink(
+            '/nextjs/migrating-from-front-end-sites',
+            'How to migrate from Front-End Sites',
+          ),
+          simpleLink(
+            '/nextjs/environment-variables',
+            'How to set environment variables',
+          ),
+          simpleLink(
+            '/nextjs/connecting-custom-domain-name',
+            'How to connect a custom domain name',
+          ),
+        ]),
+        simpleLink('/nextjs/request-access', 'Private Alpha Program'),
+      ]),
+
       {
         link: '/guides/decoupled',
         title: 'Front-End Sites and Starter Kits',
