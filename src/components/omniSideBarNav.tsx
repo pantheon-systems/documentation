09384db0339d9fbc3254fca5b7ad8508d84b706e
Steve Persch <steve.persch@pantheon.io>
2025-12-30 17:42:49 -0600

    removing unused code

--- src/components/omniSideBarNav.tsx
+++ src/components/omniSideBarNav.tsx
@@ -1,8 +1,5 @@
 "use server";
-import {
-  findSubMenuItemsToUse,
-  getOmniSidebarActiveSection,
-} from "./omni-components/helpers";
+import { getOmniSidebarActiveSection } from "./omni-components/helpers";
 
 import { SideNavCompact } from "./ui/pds-re-export";
 import { MOBILE_MENU_BREAKPOINT } from "@/constants";
@@ -11,41 +8,14 @@ import { turnItemsIntoLinks } from "./omni-components/client-helper";
 
 export const OmniSidebarNav = async ({
   activePage,
-  submenuPathToUse = "",
 }: {
   activePage: string;
-  submenuPathToUse?: string;
 }) => {
   const OmniItems = await getOmniItems();
 
-  /* Reporting debug code
-  const flattenedOmniItems = flattenOmniItems(OmniItems);
-  const results = CalculateFilteredPathsInMenu(
-    filteredWrittenPaths,
-    flattenedOmniItems,
-);
-  console.log(results);
-  */
-
   const menuItems = getOmniSidebarActiveSection(activePage, OmniItems);
 
-  // If the caller is asking for a specific submenu, use that directly.
-  if (submenuPathToUse.length > 0) {
-    const submenuItems = findSubMenuItemsToUse(submenuPathToUse, OmniItems);
-    if (!submenuItems) {
-      return null;
-    }
-    const submenuLinks = turnItemsIntoLinks(submenuItems, activePage);
-    return (
-      <SideNavCompact
-        mobileMenuMaxWidth={MOBILE_MENU_BREAKPOINT}
-        className="sidenav-compact"
-        ariaLabel={submenuLinks.linkContent}
-        headingText={submenuLinks.linkContent}
-        menuItems={submenuLinks.links}
-      />
-    );
-  } else if (menuItems) {
+  if (menuItems) {
     const OmniLinks = turnItemsIntoLinks(menuItems, activePage);
     return (
       <SideNavCompact
