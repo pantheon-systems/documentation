7b43f24befc3d6187a14f8346e82104f7ac1228c
Rachel <rachel@pantheon.io>
2026-01-21 13:51:11 -0600

    Switch header for incoming protocol (#9837)
    * Switch header for incoming protocol
    
    * Update policy-doc-surrogate-key

--- src/middleware.ts
+++ src/middleware.ts
@@ -228,8 +228,8 @@ export function middleware(request: NextRequest) {
   // See https://github.com/pantheon-systems/documentation/issues/9791
   // for more context.
   const siteMachineName = process.env.PANTHEON_SITE_MACHINE_NAME || "documentation-in-nextjs";
-  const incomingProtocol = request.headers.get('x-proto') || '';
-  const policyDocSurrogateKey = request.headers.get('policy-doc-surrogate-key') || '';
+  const incomingProtocol = request.headers.get('x-forwarded-proto') || '';
+  const policyDocSurrogateKey = request.headers.get('Policy-Doc-Surrogate-Key') || '';
   if (incomingProtocol === 'http' && policyDocSurrogateKey) {
       url.protocol = "https:";
       url.hostname = policyDocSurrogateKey;
