6a64d35ad20be987611a7b9f911e5aeff8d2e747
Steve Persch <steve.persch@pantheon.io>
2026-03-08 15:41:21 -0500

    Setting much longer playwright timeout

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  timeout: 10000,
+  timeout: 45000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     video: {
