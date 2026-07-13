1317169057838c6998a730e1a4f9a6215d734843
Chris Reynolds <chris@jazzsequence.com>
2026-07-13 14:37:21 -0600

    point faq section in beta docs to main faq

--- src/middleware.ts
+++ src/middleware.ts
@@ -205,6 +205,7 @@ const RedirectMap: Record<string, string> = {
   "/guides/github-application/support-considerations": "/guides/external-repositories/support-considerations",
   "/nextjs/considerations": "/nextjs/comparison-to-cms-hosting",
   "/guides/global-cdn/global-cdn-beta": "/guides/global-cdn/next-gen-global-cdn",
+  "/guides/global-cdn/global-cdn-beta#faq": "/guides/global-cdn/global-cdn-faq",
 
   // These are redirects that were not in AGCDN but discovered in the
   // process of migrating the site to Next.js.
