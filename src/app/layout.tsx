39a71ba0481cde897342364202206cbd5a90fb0d
Rachel Whitton <rachel@pantheon.io>
2026-08-05 17:17:32 -0500

    Remove GA4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { Suspense } from "react";
 
 import "@/styles/globals.css";
 import { PropsWithChildren } from "react";
-import { AnalyticsGA4 } from "./analytics-ga4";
 import { AnalyticsGTM } from "./analytics-gtm";
 import SegmentAnalytics from "./analytics-segment";
 
@@ -26,31 +25,13 @@ const sourceCodePro = Source_Code_Pro({
 function MyApp({ children }: PropsWithChildren) {
   const enableAnalytics = process.env.NODE_ENV !== "development";
 
-  const enableGA4 = enableAnalytics && process.env.NEXT_PUBLIC_GA_ID;
   const enableGTM =
-    enableAnalytics && !enableGA4 && process.env.NEXT_PUBLIC_GTM_ID;
+    enableAnalytics && process.env.NEXT_PUBLIC_GTM_ID;
 
   return (
     <html>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
-        {/* Google Analytics: Replace XXXXXXXXXX with your google analytics id and uncomment the following code. */}
-        {enableGA4 && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-              {`
-				window.dataLayer = window.dataLayer || [];
-				function gtag(){window.dataLayer.push(arguments);}
-				gtag('js', new Date());
-				gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-			`}
-            </Script>
-          </>
-        )}
 
         {enableGTM && (
           <Script
@@ -74,9 +55,6 @@ function MyApp({ children }: PropsWithChildren) {
           data-domain-script="fba5027b-04c0-4165-8778-4e10fb9f5fa3"
         />
       </head>
-      {process.env.NEXT_PUBLIC_GA_ID && typeof window !== "undefined" && (
-        <AnalyticsGA4 />
-      )}
       {process.env.NEXT_PUBLIC_GTM_ID && (
         <AnalyticsGTM />
       )}
