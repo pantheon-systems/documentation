71422d12b7d7511c88fce40fc7d2a3611065cbde
Rachel Whitton <rachel@pantheon.io>
2026-08-25 13:50:06 -0500

    fix page

--- tests/playwright/tests/release-notes.spec.ts
+++ tests/playwright/tests/release-notes.spec.ts
@@ -51,7 +51,7 @@ test('Direct navigation to paginated category page contains expected release not
 // verify that categories are persisted when using pagination links
 test('clicking pagination works when already filtered by a category', async ({ page }) => {
   await page.goto('/release-notes/1?category=drupal');
-  await page.getByRole('button', { name: '3', exact: true }).click();
+  await page.getByRole('button', { name: '4', exact: true }).click();
   await page.waitForTimeout(1000);
   await expect(page).toHaveURL('/release-notes/4?category=drupal');
 
